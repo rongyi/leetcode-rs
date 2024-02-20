8ab94d8e481790d62fb8a1f755a3ca331073450b
rongyi <rongyi@onchain.com>
2024-02-20 11:05:59 +0800

    Add comment

--- src/lc343.rs
+++ src/lc343.rs
@@ -7,6 +7,8 @@ impl Solution {
         dp[1] = 1;
         for i in 2..=n {
             for j in 1..i {
+                // split i -> j and i - j, and split (i - j) to smaller parts,
+                // and its max value can be get is dp[i - j]
                 dp[i] = dp[i].max(i32::max((j * (i - j)) as i32, j as i32 * dp[i - j]))
             }
         }
