537635cfa676f75ebc234f4e46f83302bbdecfc3
rongyi <rongyi@onchain.com>
2024-02-22 16:15:55 +0800

    Update code

---
+++ src/lc371.rs
@@ -0,0 +1,22 @@
+struct Solution;
+
+impl Solution {
+    // do not use + or -
+    pub fn get_sum(a: i32, b: i32) -> i32 {
+        let mut carry = 0;
+        let mut ret = 0;
+        let mut mask = 1u32;
+        while mask != 0 {
+            ret |= (a ^ b ^ carry) & mask as i32;
+            carry = ((a & b) | ((a ^ b) & carry)) << 1;
+
+            mask = mask << 1;
+        }
+
+
+        ret
+    }
+}
+
+
+fn main() {}
