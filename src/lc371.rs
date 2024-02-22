b9ef994f8158ed798f37eb4e303830e7282def4a
rongyi <rongyi@onchain.com>
2024-02-22 16:29:29 +0800

    Update

--- src/lc371.rs
+++ src/lc371.rs
@@ -7,16 +7,16 @@ impl Solution {
         let mut ret = 0;
         let mut mask = 1u32;
         while mask != 0 {
+            // use ^ to mimic add
             ret |= (a ^ b ^ carry) & mask as i32;
+            // carry for case 1 + 1 or (1 + 0) / (0 + 1) with carry == 1
             carry = ((a & b) | ((a ^ b) & carry)) << 1;
 
             mask = mask << 1;
         }
 
-
         ret
     }
 }
-
 
 fn main() {}
