6c5a108556fc29c1c32d9f24464b13a63b0ca7b6
rongyi <rongyi@onchain.com>
2024-03-02 08:22:28 +0800

    Update

--- src/lc435.rs
+++ src/lc435.rs
@@ -9,9 +9,13 @@ impl Solution {
             // oh shit, we are covered by prev range!
             if intervals[i][0] < intervals[prev][1] {
                 ret += 1;
+                // 拿掉横跨最靠前的那个罩子，此时prev指向当前
+                // 当前的区间完全被cover了，删掉最靠前的那个，此时prev有变化
+                // prev指向当前这个区间
                 if intervals[i][1] < intervals[prev][1] {
                     prev = i;
                 }
+                // 最靠前的区间就是当前区间，我们干掉这个罩子，prev没有变化
             } else {
                 prev = i;
             }
