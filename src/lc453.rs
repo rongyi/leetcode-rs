59b1727e48ef87982d566430a3ce8c2cea8dca3a
rongyi <rongyi@onchain.com>
2024-03-05 17:39:16 +0800

    Update

--- src/lc453.rs
+++ src/lc453.rs
@@ -1,6 +1,8 @@
 struct Solution;
 
 impl Solution {
+    // it's a math question
+    // https://leetcode.com/problems/minimum-moves-to-equal-array-elements/discuss/93817/It-is-a-math-question
     pub fn min_moves(nums: Vec<i32>) -> i32 {
         let nums: Vec<i64> = nums.iter().map(|&i| i as i64).collect();
         let a = nums.iter().fold(0i64, |acc, &cur| acc + cur);
