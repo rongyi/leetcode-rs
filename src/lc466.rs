9e2fa3af62dd21aab70c7afcda0b440ca2ca6881
rongyi <rongyi@onchain.com>
2024-03-09 09:33:29 +0800

    Add 466

---
+++ src/lc466.rs
@@ -0,0 +1,34 @@
+struct Solution;
+
+impl Solution {
+    pub fn get_max_repetitions(s1: String, n1: i32, s2: String, n2: i32) -> i32 {
+        let s1: Vec<char> = s1.chars().collect();
+        let s2: Vec<char> = s2.chars().collect();
+        let len1 = s1.len();
+        let len2 = s2.len();
+        let mut count1 = 0;
+        let mut count2 = 0;
+        let mut i = 0;
+        let mut j = 0;
+
+        // like subsequnce
+        while count1 < n1 {
+            if s1[i] == s2[j] {
+                j += 1;
+                if j == len2 {
+                    j = 0;
+                    count2 += 1;
+                }
+            }
+            i += 1;
+            if i == len1 {
+                i = 0;
+                count1 += 1;
+            }
+        }
+
+        count2 / n2
+    }
+}
+
+fn main() {}
