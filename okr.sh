c28cbf101aed61a7a6a5b8809ced2c71e793185e
rongyi <rongyi@onchain.com>
2024-03-09 11:19:06 +0800

    Update okr

--- okr.sh
+++ okr.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-git log --since=midnight --name-only --pretty=format:'' | grep -v '^$'
+git log --since=midnight --name-only --pretty=format:'' | grep -v '^$' | sort |uniq
 
 
-cnt=`git log --since=midnight --name-only --pretty=format:'' | grep -v '^$' |grep '.rs$' | wc -l`
+cnt=`git log --since=midnight --name-only --pretty=format:'' | grep -v '^$' |grep '.rs$' |sort |uniq | wc -l`
 echo "today you finished $cnt leetcode in rust, happy coding ry!"
 
 #git log --since='7 days ago' --name-only --pretty=format:'' | grep -v '^$' |grep '.rs$'
 
 
-cnt=`git log --since='7 days ago' --name-only --pretty=format:'' | grep -v '^$' |grep '.rs$'| wc -l`
+cnt=`git log --since='7 days ago' --name-only --pretty=format:'' | grep -v '^$' |grep '.rs$' |sort|uniq| wc -l`
 echo "last 7 days you finished $cnt leetcode in rust, happy coding ry!"
