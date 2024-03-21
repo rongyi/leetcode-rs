109a1daf2ac6281d6c37a134ceac35903d199a20
rongyi <rongyi@onchain.com>
2024-03-21 15:35:13 +0800

    Update 521

---
+++ src/lc521.rs
@@ -0,0 +1,12 @@
+struct Solution;
+
+impl Solution {
+    pub fn find_lu_slength(a: String, b: String) -> i32 {
+        if a == b {
+            return -1;
+        }
+        a.len().max(b.len()) as i32
+    }
+}
+
+fn main() {}
