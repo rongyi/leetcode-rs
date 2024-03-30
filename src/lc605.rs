62a9eee6aebd14fdd05d2bad01262b351b46b423
rongyi <rongyi@onchain.com>
2024-03-30 11:14:46 +0800

    Update

--- src/lc605.rs
+++ src/lc605.rs
@@ -4,6 +4,7 @@ impl Solution {
     pub fn can_place_flowers(mut flowerbed: Vec<i32>, n: i32) -> bool {
         let mut cnt = 0;
         let mut i = 0;
+        // just put there
         while i < flowerbed.len() {
             if flowerbed[i] == 0 {
                 if (i == 0 || flowerbed[i - 1] == 0)
