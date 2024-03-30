b9f3db4534b5322ad83d8966048df4403ed46a05
rongyi <rongyi@onchain.com>
2024-03-30 10:51:47 +0800

    Updte 605

---
+++ src/lc605.rs
@@ -0,0 +1,23 @@
+struct Solution;
+
+impl Solution {
+    pub fn can_place_flowers(mut flowerbed: Vec<i32>, n: i32) -> bool {
+        let mut cnt = 0;
+        let mut i = 0;
+        while i < flowerbed.len() {
+            if flowerbed[i] == 0 {
+                if (i == 0 || flowerbed[i - 1] == 0)
+                    && (i == flowerbed.len() - 1 || flowerbed[i + 1] == 0)
+                {
+                    cnt += 1;
+                    flowerbed[i] = 1;
+                }
+            }
+
+            i += 1;
+        }
+        cnt >= n
+    }
+}
+
+fn main() {}
