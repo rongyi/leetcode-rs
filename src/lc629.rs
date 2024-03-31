2c44e41e16e40d2832eca60227bd950f131d6620
rongyi <rongyi@onchain.com>
2024-03-31 11:42:53 +0800

    Update

--- src/lc629.rs
+++ src/lc629.rs
@@ -3,13 +3,6 @@ struct Solution;
 impl Solution {
     pub fn k_inverse_pairs(n: i32, k: i32) -> i32 {
         let prime = 1e9 as i64 + 7;
-        let sum = n * (n - 1) / 2;
-        if k > sum || k < 0 {
-            return 0;
-        }
-        if k == 0 || k == sum {
-            return 1;
-        }
 
         // dp[n][k] denotes the number of arrays that have k inverse pairs for array composed of 1 to n
         // we can establish the recursive relationship between dp[n][k] and dp[n-1][i]:
@@ -20,21 +13,20 @@ impl Solution {
         // if we put n as the first number then there's n-1 inverse pairs involve n so the rest k-(n-1) comes from the first n-1 numbers
 
         // dp[n][k] = dp[n-1][k]+dp[n-1][k-1]+dp[n-1][k-2]+...+dp[n-1][k+1-n+1]+dp[n-1][k-n+1]
-        let mut dp = vec![vec![0i64; k as usize + 1]; n as usize];
+        let mut dp = vec![vec![0i64; k as usize + 1]; n as usize + 1];
         dp[0][0] = 1;
-        for i in 1..n as usize {
+        for i in 1..=n as usize {
             for j in 0..=k as usize {
-                let mut m = j as i32;
-                while m >= 0 && m >= (j - i) as i32 {
-                    dp[i][j] += dp[i - 1][m as usize];
-                    m -= 1;
+                // upper bound for m is min(j, i - 1)
+                // i - 1 is the last case, put largest num in front to make i - 1 inverse array
+                for m in 0..=j.min(i - 1) {
+                    dp[i][j] += dp[i - 1][j - m];
                 }
-
                 dp[i][j] %= prime;
             }
         }
 
-        dp[n as usize - 1][k as usize] as i32
+        dp[n as usize][k as usize] as i32
     }
 }
 
