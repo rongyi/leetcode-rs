5062c20ce3db95e1671fb35176eb83616cb1fa02
rongyi <rongyi@onchain.com>
2024-03-31 10:43:42 +0800

    Update 629

--- src/lc629.rs
+++ src/lc629.rs
@@ -10,6 +10,16 @@ impl Solution {
         if k == 0 || k == sum {
             return 1;
         }
+
+        // dp[n][k] denotes the number of arrays that have k inverse pairs for array composed of 1 to n
+        // we can establish the recursive relationship between dp[n][k] and dp[n-1][i]:
+
+        // if we put n as the last number then all the k inverse pair should come from the first n-1 numbers
+        // if we put n as the second last number then there's 1 inverse pair involves n so the rest k-1 comes from the first n-1 numbers
+        // ...
+        // if we put n as the first number then there's n-1 inverse pairs involve n so the rest k-(n-1) comes from the first n-1 numbers
+
+        // dp[n][k] = dp[n-1][k]+dp[n-1][k-1]+dp[n-1][k-2]+...+dp[n-1][k+1-n+1]+dp[n-1][k-n+1]
         let mut dp = vec![vec![0i64; k as usize + 1]; n as usize];
         dp[0][0] = 1;
         for i in 1..n as usize {
