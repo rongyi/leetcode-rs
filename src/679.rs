532e8b2fe3eec37736f99fe00531d7230f9fe71d
rongyi <rongyi@onchain.com>
2024-04-15 14:55:29 +0800

    Update

---
+++ src/679.rs
@@ -0,0 +1,57 @@
+#![allow(dead_code)]
+struct Solution;
+
+const TARGET: f64 = 24.0;
+const EPSILON: f64 = 0.001;
+impl Solution {
+    pub fn judge_point24(cards: Vec<i32>) -> bool {
+        let nums: Vec<f64> = cards.into_iter().map(|v| v as f64).collect();
+
+        Self::solve(&nums)
+    }
+    fn solve(nums: &Vec<f64>) -> bool {
+        if nums.len() == 1 {
+            return (nums[0] - TARGET).abs() < EPSILON;
+        }
+
+        for i in 0..nums.len() {
+            for j in 0..nums.len() {
+                if i == j {
+                    continue;
+                }
+                let mut new_nums = Vec::new();
+                for k in 0..nums.len() {
+                    if k != i && k != j {
+                        new_nums.push(nums[k]);
+                    }
+                }
+
+                for &op in &['+', '-', '*', '/'] {
+                    if (op == '+' || op == '*') && i > j {
+                        continue;
+                    }
+                    if op == '/' && nums[j].abs() < EPSILON {
+                        continue;
+                    }
+                    let ret = match op {
+                        '+' => nums[i] + nums[j],
+                        '-' => nums[i] - nums[j],
+                        '*' => nums[i] * nums[j],
+                        '/' => nums[i] / nums[j],
+                        _ => unreachable!(),
+                    };
+                    new_nums.push(ret);
+                    if Self::solve(&new_nums) {
+                        return true;
+                    }
+
+                    new_nums.pop();
+                }
+            }
+        }
+
+        false
+    }
+}
+
+fn main() {}
