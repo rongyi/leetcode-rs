eca8d5683864628e441fabfa729e45a6fa93c847
rongyi <rongyi@onchain.com>
2024-04-16 15:09:33 +0800

    Update

--- src/686.rs
+++ src/686.rs
@@ -5,6 +5,10 @@ impl Solution {
     pub fn repeated_string_match(a: String, b: String) -> i32 {
         let mut acp = a.clone();
         let mut ret = 1;
+        // 1. when b is so small, that we only need double a, if we can not find b in doubled a thats it
+        // 2. when b.len > a.len, we need this time to let a's len catch up b len
+        // then we double it to search that
+        // so both case we get condition:
         while ret <= (b.len() / a.len()) as i32 + 2 {
             if acp.find(&b).is_some() {
                 return ret;
