9f97dc144e516efc7886f6e43b8a18e2eea2f8e5
rongyi <rongyi@onchain.com>
2024-05-05 16:13:42 +0800

    Update

--- src/769.rs
+++ src/769.rs
@@ -8,7 +8,11 @@ impl Solution {
 
         let mut cur_max = 0;
         for i in 0..arr.len() {
+            // The basic idea is to use max[] array to keep track of the max value until the current position,
+            // and compare it to the sorted array (indexes from 0 to arr.length - 1). If the max[i] equals the
+            // element at index i in the sorted array, then the final count++.
             cur_max = cur_max.max(arr[i]);
+            // here i means in sorted array to here the max value should be i
             if i as i32 == cur_max {
                 ret += 1;
             }
