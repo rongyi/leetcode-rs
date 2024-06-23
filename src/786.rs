6a0e5110b1f9bc74c51024f4a0dc9db318f3e354
rongyi <sqrongyi@163.com>
2024-06-23 17:14:45 +0800

    Update lc

---
+++ src/786.rs
@@ -0,0 +1,54 @@
+#![allow(dead_code)]
+
+struct Solution;
+
+use std::collections::BinaryHeap;
+
+#[derive(PartialEq, Debug)]
+struct Fraction {
+    f: f64,
+    numerator: usize,
+    demoninator: usize,
+}
+
+impl Eq for Fraction {}
+
+impl PartialOrd for Fraction {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        other.f.partial_cmp(&self.f)
+    }
+}
+
+impl Ord for Fraction {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.f.partial_cmp(&self.f).unwrap()
+    }
+}
+
+impl Solution {
+    pub fn kth_smallest_prime_fraction(arr: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut pq: BinaryHeap<Fraction> = BinaryHeap::new();
+        let sz = arr.len();
+        for i in 0..sz - 1 {
+            pq.push(Fraction {
+                f: arr[i] as f64 / arr[sz - 1] as f64,
+                numerator: i,
+                demoninator: sz - 1,
+            });
+        }
+        for _ in 0..k - 1 {
+            let mut cur = pq.pop().unwrap();
+            cur.demoninator -= 1;
+            pq.push(Fraction {
+                f: arr[cur.numerator] as f64 / arr[cur.demoninator] as f64,
+                numerator: cur.numerator,
+                demoninator: cur.demoninator,
+            })
+        }
+
+        let cur = pq.pop().unwrap();
+        vec![arr[cur.numerator], arr[cur.demoninator]]
+    }
+}
+
+fn main() {}
