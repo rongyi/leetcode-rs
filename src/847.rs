a7d34318d8b233507d2e1fafb7461d0a888eaba1
rongyi <sqrongyi@163.com>
2024-07-19 07:34:52 +0800

    update

--- src/847.rs
+++ src/847.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
-
 struct Solution;
+
+use std::collections::VecDeque;
+
 
 impl Solution {
     pub fn shortest_path_length(graph: Vec<Vec<i32>>) -> i32 {
@@ -11,18 +13,19 @@ impl Solution {
         // all mask eq to 1
         let success = (1 << sz) - 1;
         let mut visited = vec![vec![false; 1 << sz]; sz];
-        let mut frontier = Vec::new();
+        let mut q: VecDeque<(usize, usize)> = VecDeque::new();
 
         for i in 0..sz {
-            frontier.push((i, 1 << i));
+            q.push_back((i, 1 << i));
             visited[i][1 << i] = true;
         }
 
         let mut step = 0;
 
-        while !frontier.is_empty() {
-            let mut new_frontier = Vec::new();
-            for &(node, mask) in frontier.iter() {
+        while !q.is_empty() {
+            let sz = q.len();
+            for _ in 0..sz {
+                let (node, mask) = q.pop_front().unwrap();
                 for &node_next in graph[node].iter() {
                     let node_next = node_next as usize;
                     let mask_next = mask | (1 << node_next);
@@ -32,12 +35,11 @@ impl Solution {
 
                     if !visited[node_next][mask_next] {
                         visited[node_next][mask_next] = true;
-                        new_frontier.push((node_next, mask_next));
+                        q.push_back((node_next, mask_next));
                     }
                 }
             }
 
-            frontier = new_frontier;
             step += 1;
         }
 
