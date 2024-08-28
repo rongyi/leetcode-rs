5e16662576146e9a8083b4bd864e2afd49d7f3e6
rongyi <sqrongyi@163.com>
2024-08-28 11:16:55 +0800

    Update 853

--- src/853.rs
+++ src/853.rs
@@ -1,7 +1,6 @@
 struct Solution;
 use std::collections::BTreeMap;
 
-
 impl Solution {
     pub fn car_fleet(target: i32, position: Vec<i32>, speed: Vec<i32>) -> i32 {
         let mut durations: BTreeMap<i32, f64> = BTreeMap::new();
@@ -15,6 +14,8 @@ impl Solution {
         let mut cur: f64 = 0.0;
         for (_, &v) in durations.iter() {
             // If a car takes longer to reach the target than the car in front of it, it forms a new fleet.
+            // and if v <= cur, means current car will meet some car in front of it, which car?
+            // we don't care, just some car in front of this car, and we dont need to distinguish this
             if v > cur {
                 cur = v;
                 ret += 1;
