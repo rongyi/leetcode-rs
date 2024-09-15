9c7d871808b96bd4704da88a459c1b8db6c54504
rongyi <sqrongyi@163.com>
2024-09-15 10:56:25 +0800

    Update

--- src/910.rs
+++ src/910.rs
@@ -1,18 +1,40 @@
+use std::collections::HashMap;
+
 struct Solution;
 
-impl Solution {
-    pub fn smallest_range_ii(mut nums: Vec<i32>, k: i32) -> i32 {
-        nums.sort_unstable();
-        let left = nums[0] + k;
-        let right = nums.last().unwrap() - k;
-        let mut ret = nums.last().unwrap() - nums.first().unwrap();
-        for i in 0..nums.len() - 1 {
-            let cur_max = right.max(nums[i] + k);
-            let cur_min = left.min(nums[i + 1] - k);
-            ret = ret.min(cur_max - cur_min);
+struct TopVotedCandidate {
+    times: Vec<i32>,
+    leaders: Vec<i32>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl TopVotedCandidate {
+    fn new(persons: Vec<i32>, times: Vec<i32>) -> Self {
+        let mut vote_cnt: HashMap<i32, i32> = HashMap::new();
+        let mut max_votes = 0;
+        let mut cur_leader = -1;
+        let mut leaders = Vec::with_capacity(persons.len());
+        for &p in persons.iter() {
+            let cur = vote_cnt.entry(p).or_insert(0);
+            *cur += 1;
+            if *cur >= max_votes {
+                max_votes = *cur;
+                cur_leader = p;
+            }
+            leaders.push(cur_leader);
         }
 
-        ret
+        Self { times, leaders }
+    }
+
+    fn q(&self, t: i32) -> i32 {
+        match self.times.binary_search(&t) {
+            Ok(idx) => self.leaders[idx],
+            Err(idx) => self.leaders[idx - 1],
+        }
     }
 }
 
