15cb261bdcc0e526c95059591b1ca09f37e9bbf7
rongyi <sqrongyi@163.com>
2024-09-16 15:45:58 +0800

    Update

---
+++ src/914.rs
@@ -0,0 +1,28 @@
+struct Solution;
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn has_groups_size_x(deck: Vec<i32>) -> bool {
+        let mut cnt = HashMap::new();
+        for card in deck.into_iter() {
+            *cnt.entry(card).or_insert(0) += 1;
+        }
+
+        let mut val = 0;
+        for &c in cnt.values() {
+            val = Self::gcd(val, c);
+        }
+
+        val > 1
+    }
+
+    fn gcd(a: i32, b: i32) -> i32 {
+        if b == 0 {
+            a
+        } else {
+            Self::gcd(b, a % b)
+        }
+    }
+}
+
+fn main() {}
