a036354bc790402ac9d384e714779145d6fea534
rongyi <sqrongyi@163.com>
2024-10-04 10:14:05 +0800

    Update

---
+++ src/966.rs
@@ -0,0 +1,47 @@
+struct Solution;
+
+use std::collections::{HashMap, HashSet};
+impl Solution {
+    pub fn spellchecker(wordlist: Vec<String>, queries: Vec<String>) -> Vec<String> {
+        let wordset: HashSet<String> = wordlist.iter().cloned().collect();
+        let mut cap_map: HashMap<String, String> = HashMap::new();
+        let mut vowel_map: HashMap<String, String> = HashMap::new();
+
+        for word in wordlist.iter() {
+            let lower = word.to_lowercase();
+            let devowel = Self::remove_vowels(&lower);
+
+            cap_map.entry(lower).or_insert(word.clone());
+            vowel_map.entry(devowel).or_insert(word.clone());
+        }
+
+        queries
+            .into_iter()
+            .map(|query| {
+                if wordset.contains(&query) {
+                    query
+                } else {
+                    let lower = query.to_lowercase();
+                    let devowel = Self::remove_vowels(&lower);
+
+                    cap_map
+                        .get(&lower)
+                        .or_else(|| vowel_map.get(&devowel))
+                        .cloned()
+                        .unwrap_or_else(|| String::from(""))
+                }
+            })
+            .collect()
+    }
+
+    fn remove_vowels(s: &str) -> String {
+        s.chars()
+            .map(|c| match c {
+                'a' | 'e' | 'i' | 'o' | 'u' => '_',
+                _ => c,
+            })
+            .collect()
+    }
+}
+
+fn main() {}
