0c211cfd147606af5f2710cf59399b973942abd8
rongyi <sqrongyi@163.com>
2024-10-06 08:17:30 +0800

    Updae

--- src/974.rs
+++ src/974.rs
@@ -1,6 +1,11 @@
 struct Solution;
 
 impl Solution {
+    // About the problems - sum of contiguous subarray , prefix sum is a common technique.
+    // Another thing is if sum[0, i] % K == sum[0, j] % K, sum[i + 1, j] is divisible by by K.
+    // So for current index j, we need to find out how many index i (i < j) exist that has the same mod of K.
+    // Now it easy to come up with HashMap <mod, frequency>
+
     pub fn subarrays_div_by_k(nums: Vec<i32>, k: i32) -> i32 {
         let mut cnt = vec![0; k as usize];
         cnt[0] = 1;
