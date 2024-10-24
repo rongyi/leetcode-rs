a795be1fedca754c8ae4319cce37d72732df8d30
rongyi <sqrongyi@163.com>
2024-10-24 14:57:14 +0800

    Add 1002

--- src/1002.rs
+++ src/1002.rs
@@ -1,33 +1,58 @@
-
 struct Solution;
 
-use std::collections::HashMap;
 impl Solution {
-    pub fn common_chars(words: Vec<String>) -> Vec<String> {
-        let word_set: Vec<HashMap<char, usize>> = words
-            .into_iter()
-            .map(|s| {
-                s.chars().fold(HashMap::new(), |mut acc, cur| {
-                    *acc.entry(cur).or_insert(0) += 1;
-                    acc
-                })
-            })
+    pub fn num_dup_digits_at_most_n(n: i32) -> i32 {
+        let digits: Vec<i32> = n
+            .to_string()
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as i32)
             .collect();
-        let mut ret = Vec::new();
-        for c in 'a'..='z' {
-            let vals: Vec<usize> = word_set
-                .iter()
-                .map(|set| set.get(&c).unwrap_or(&0))
-                .copied()
-                .collect();
-            let &val = vals.iter().min().unwrap();
-            for _ in 0..val {
-                ret.push(c.to_string());
+        let k = digits.len();
+
+        let mut ret = 0;
+
+        // count all uniq which is less than k wide
+        for i in 1..k {
+            // first digit can not select 0, so permutation is 9
+            // after this, it's 9 * 8 * 7 * 6 ...
+            ret += 9 * Self::perm(9, i as i32 - 1);
+        }
+        let mut used = vec![false; 10];
+        // prefix
+        // e.g. 8625, we calculate 1XXX -> 7XXX uniq
+        //                         80XX -> 85XX
+        for i in 0..k {
+            for j in (if i > 0 { 0 } else { 1 })..digits[i] {
+                if !used[j as usize] {
+                    ret += Self::perm(10 - (i as i32 + 1), k as i32 - (i as i32 + 1));
+                }
+            }
+            if used[digits[i] as usize] {
+                break;
+            }
+            used[digits[i] as usize] = true;
+
+            // when we come here we know this number n is also a uniq number
+            if i == k - 1 {
+                ret += 1;
             }
         }
 
-        ret
+        n - ret
+    }
+
+    fn perm(init: i32, cnt: i32) -> i32 {
+        (0..cnt).fold(1, |mut acc, i| {
+            acc *= init - i;
+            acc
+        })
     }
 }
 
-fn main() {}
+fn main() {
+    let val = (0..0).fold(1, |mut acc, cur| {
+        acc += cur;
+        acc
+    });
+    println!("val{}", val);
+}
