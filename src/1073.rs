3a91fab82d492071131b56ee8a1bee75638fc2c4
rongyi <sqrongyi@163.com>
2024-11-07 10:54:27 +0800

    Update 1073, make shit more concise

--- src/1073.rs
+++ src/1073.rs
@@ -7,24 +7,16 @@ impl Solution {
         let mut i = arr1.len() as i32 - 1;
         let mut j = arr2.len() as i32 - 1;
         while i >= 0 || j >= 0 || carry != 0 {
-            let mut sum = carry;
-
             if i >= 0 {
-                sum += arr1[i as usize];
+                carry += arr1[i as usize];
                 i -= 1;
             }
             if j >= 0 {
-                sum += arr2[j as usize];
+                carry += arr2[j as usize];
                 j -= 1;
             }
-            ret.push(sum.abs() % 2);
-            carry = if sum < 0 {
-                1
-            } else if sum > 1 {
-                -1
-            } else {
-                0
-            };
+            ret.push(if carry.abs() & 1 == 1 { 1 } else { 0 });
+            carry = -(carry >> 1);
         }
         while ret.len() > 1 && ret.last() == Some(&0) {
             ret.pop();
