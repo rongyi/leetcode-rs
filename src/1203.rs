768e123c8f65c0ec644fb7761bf47325b8e57e7f
rongyi <sqrongyi@163.com>
2024-12-19 16:26:22 +0800

    Update 1203

--- src/1203.rs
+++ src/1203.rs
@@ -1,75 +1,78 @@
 #![allow(dead_code)]
 
+
 struct Solution;
 
 use std::collections::VecDeque;
 impl Solution {
     pub fn sort_items(n: i32, m: i32, group: Vec<i32>, before_items: Vec<Vec<i32>>) -> Vec<i32> {
+        // n nodes
         let n = n as usize;
+        // m groups
         let m = m as usize;
-        // reformat group, make orhan group a uniq group number
+        // make groupless item a uniq new group id, 0..=m-1 is reserved for real group, only after
         let group: Vec<usize> = group
             .into_iter()
             .enumerate()
             .map(|(i, g)| if g == -1 { i + m } else { g as usize })
             .collect();
-
+        let mut group_indegree = vec![0; m + n];
         let mut group_graph: Vec<Vec<usize>> = vec![vec![]; m + n];
-        let mut group_in_degress = vec![0; m + n];
 
+        let mut item_indegree: Vec<i32> = vec![0; n];
         let mut item_graph: Vec<Vec<usize>> = vec![vec![]; n];
-        let mut item_in_degress = vec![0; n];
 
-        // build graph: deps should before cur_item
-        for (cur_item, deps) in before_items.iter().enumerate() {
-            for &prev_item in deps.iter() {
+        // construct relationship
+        for (cur_item, prev_items) in before_items.iter().enumerate() {
+            for &prev_item in prev_items.iter() {
                 let prev_item = prev_item as usize;
                 // same group?
-                if group[cur_item] == group[prev_item] {
+                if group[prev_item] == group[cur_item] {
                     item_graph[prev_item].push(cur_item);
-                    item_in_degress[cur_item] += 1;
+                    item_indegree[cur_item] += 1;
                 } else {
+                    group_indegree[group[cur_item]] += 1;
                     group_graph[group[prev_item]].push(group[cur_item]);
-                    group_in_degress[group[cur_item]] += 1;
                 }
             }
         }
-        let group_order = Self::topological_sort(&group_graph, &mut group_in_degress);
+        let group_order = Self::topology_sort(&group_graph, &mut group_indegree);
         if group_order.is_empty() {
-            return Vec::new();
+            return vec![];
         }
-        let item_order = Self::topological_sort(&item_graph, &mut item_in_degress);
+        let item_order = Self::topology_sort(&item_graph, &mut item_indegree);
         if item_order.is_empty() {
             return vec![];
         }
-
-        let mut group_items: Vec<Vec<i32>> = vec![vec![]; m + n];
-        for &item in &item_order {
-            group_items[group[item]].push(item as i32);
+        // so now we ca group item in group and insert to a ret by group order
+        let mut by_group: Vec<Vec<i32>> = vec![vec![]; m + n];
+        for &item in item_order.iter() {
+            by_group[group[item]].push(item as i32);
         }
         let mut ret = Vec::new();
-        for &group_id in group_order.iter() {
-            ret.extend(&group_items[group_id]);
+        for &gid in group_order.iter() {
+            ret.extend(&by_group[gid]);
         }
 
         ret
     }
 
-    fn topological_sort(graph: &Vec<Vec<usize>>, indegree: &mut Vec<i32>) -> Vec<usize> {
+    // start from indegree 0 and go
+    fn topology_sort(graph: &Vec<Vec<usize>>, indegree: &mut Vec<i32>) -> Vec<usize> {
         let sz = graph.len();
-        let mut ret = Vec::new();
+        let mut q: VecDeque<usize> = VecDeque::new();
 
-        let mut q = VecDeque::new();
-        // start from 0 in degree
+        // start from 0 indegree
         for i in 0..sz {
             if indegree[i] == 0 {
                 q.push_back(i);
             }
         }
 
-        while let Some(node) = q.pop_front() {
-            ret.push(node);
-            for &next in graph[node].iter() {
+        let mut ret = Vec::new();
+        while let Some(cur) = q.pop_front() {
+            ret.push(cur);
+            for &next in graph[cur].iter() {
                 indegree[next] -= 1;
                 if indegree[next] == 0 {
                     q.push_back(next);
