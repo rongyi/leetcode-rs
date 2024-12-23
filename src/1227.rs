06238b87674c86d9d42db6dd943d24f3a68428c8
rongyi <sqrongyi@163.com>
2024-12-23 17:10:33 +0800

    Update

---
+++ src/1227.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code)]
+
+struct Solution;
+
+impl Solution {
+    pub fn nth_person_gets_nth_seat(n: i32) -> f64 {
+        if n == 1 {
+            return 1.0;
+        }
+
+        return 0.5;
+    }
+}
+
+fn main() {}
