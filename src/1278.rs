578cc768c6c9ff39ef17fa60d8444e8078e6b7e3
rongyi <sqrongyi@163.com>
2025-01-02 10:12:50 +0800

    make it more readable for 1278

--- src/1278.rs
+++ src/1278.rs
@@ -17,21 +17,25 @@ impl Solution {
                 cost[i][j] = cost[i + 1][j - 1] + if s[i] == s[j] { 0 } else { 1 };
             }
         }
-        // dp[i][j] represents min changes needed to partition s[0..i] into j+1 groups
-        let mut dp = vec![vec![i32::MAX / 2; k]; sz];
+        // dp[i][j] end to =j and split to i chunk
+        let mut dp = vec![vec![i32::MAX / 2; sz]; k + 1];
+        // Initialize for k=1
         for i in 0..sz {
-            dp[i][0] = cost[0][i] as i32;
+            // 分成一份的值就是从0 ==> i的子字符创需要改变的值
+            dp[1][i] = cost[0][i];
         }
 
-        for j in 1..k {
-            for i in j..sz {
-                for l in j - 1..i {
-                    dp[i][j] = dp[i][j].min(dp[l][j - 1] + cost[l + 1][i] as i32);
+        for k in 2..=k {
+            for i in k - 1..sz {
+                for j in ((k - 2)..=(i - 1)).rev() {
+                    // i 是终点， j是中间劈开的点, j往左移， i慢慢放开
+                    // j劈开的左边部分是 dp[k - 1][j], 右边是change[j + 1][i]
+                    dp[k][i] = dp[k][i].min(cost[j + 1][i] + dp[k - 1][j]);
                 }
             }
         }
 
-        dp[sz - 1][k - 1]
+        dp[k][sz - 1]
     }
 }
 
