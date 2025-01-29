343512f31dc5bc16fc0843139461662746967bcd
rongyi <sqrongyi@163.com>
2025-01-29 15:10:11 +0800

    Update 1346

--- src/1346.rs
+++ src/1346.rs
@@ -1,21 +1,17 @@
 #![allow(dead_code)]
 
-
 struct Solution;
 
 use std::collections::HashSet;
 impl Solution {
     pub fn check_if_exist(arr: Vec<i32>) -> bool {
-        let zero_count: usize = arr.iter().filter(|&&x| x == 0).collect::<Vec<_>>().len();
-        if zero_count >= 2 {
-            return true;
-        }
-        // or just calculate
-        let all_nums: HashSet<i32> = arr.iter().copied().collect();
+        let mut all_nums: HashSet<i32> = HashSet::new();
         for &num in arr.iter() {
-            if num != 0 && all_nums.contains(&(num * 2)) {
+            if all_nums.contains(&(num * 2)) || (num % 2 == 0 && all_nums.contains(&(num / 2))) {
                 return true;
             }
+
+            all_nums.insert(num);
         }
         false
     }
