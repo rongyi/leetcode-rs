c03295737c03f0f0637b813d80c108f4c809ca89
rongyi <sqrongyi@163.com>
2025-01-29 09:56:25 +0800

    Update

--- src/1343.rs
+++ src/1343.rs
@@ -3,25 +3,19 @@ struct Solution;
 
 impl Solution {
     pub fn num_of_subarrays(arr: Vec<i32>, k: i32, threshold: i32) -> i32 {
-        let sz = arr.len();
-        let mut prefix = vec![0; sz + 1];
-        for (i, &num) in arr.iter().enumerate() {
-            prefix[i + 1] = prefix[i] + num as i64;
-        }
+        let target = k * threshold;
         let k = k as usize;
-        let threshold = threshold as i64;
-
+        let mut win_sum: i32 = arr[..k].iter().sum();
         let mut total = 0;
-        for i in 0..sz {
-            let mut sum = prefix[i + 1];
-            if i >= k {
-                sum -= prefix[i - k + 1];
-            }
 
-            if i >= k - 1 {
-                if sum / k as i64 >= threshold {
-                    total += 1;
-                }
+        if win_sum >= target {
+            total += 1;
+        }
+        for i in k..arr.len() {
+            win_sum += arr[i];
+            win_sum -= arr[i - k];
+            if win_sum >= target {
+                total += 1;
             }
         }
 
