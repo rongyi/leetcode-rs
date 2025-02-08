b8fc769b534437cc7f43c5b61ac3492a4dd6cc81
rongyi <sqrongyi@163.com>
2025-02-08 08:13:31 +0800

    Update

--- src/1371.rs
+++ src/1371.rs
@@ -5,7 +5,6 @@ struct Solution;
 use std::collections::HashMap;
 impl Solution {
     pub fn find_the_longest_substring(s: String) -> i32 {
-        let vowels = "aoeiu";
         let mut prev: HashMap<i32, i32> = HashMap::new();
         // mask 0 prev: -1, just for (j - i)
         prev.insert(0, -1);
@@ -13,8 +12,13 @@ impl Solution {
         let mut max_len = 0;
         let mut mask = 0;
         for (idx, c) in s.chars().enumerate() {
-            if let Some(vidx) = vowels.find(c) {
-                mask ^= 1 << vidx;
+            match c {
+                'a' => mask ^= 1 << 0,
+                'o' => mask ^= 1 << 1,
+                'e' => mask ^= 1 << 2,
+                'i' => mask ^= 1 << 3,
+                'u' => mask ^= 1 << 4,
+                _ => (),
             }
             if let Some(prev_idx) = prev.get(&mask) {
                 max_len = max_len.max(idx as i32 - prev_idx);
