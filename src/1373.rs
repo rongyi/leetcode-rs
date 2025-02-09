09795290195247c67a737e28568f98f3e70ad89c
rongyi <sqrongyi@163.com>
2025-02-09 10:44:49 +0800

    add 1372

---
+++ src/1373.rs
@@ -0,0 +1,58 @@
+#![allow(dead_code)]
+
+struct Solution;
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn max_sum_bst(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut max_sum = 0;
+        if let Some(node) = root.as_ref() {
+            Self::dfs(node, &mut max_sum);
+        }
+
+        max_sum
+    }
+    // (sum, min, max)
+    fn dfs(node: &Rc<RefCell<TreeNode>>, max_sum: &mut i32) -> (i32, i32, i32) {
+        let node = node.borrow();
+        let l;
+        let r;
+        if let Some(lnode) = node.left.as_ref() {
+            l = Self::dfs(lnode, max_sum);
+        } else {
+            l = (0, node.val, node.val - 1)
+        }
+        if let Some(rnode) = node.right.as_ref() {
+            r = Self::dfs(rnode, max_sum);
+        } else {
+            r = (0, node.val + 1, node.val)
+        }
+        if l.2 < node.val && node.val < r.1 {
+            *max_sum = (*max_sum).max(node.val + l.0 + r.0);
+            return (node.val + l.0 + r.0, l.1, r.2);
+        }
+
+        (0, i32::MIN, i32::MAX)
+    }
+}
+
+fn main() {}
