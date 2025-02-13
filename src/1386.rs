f9bc835ec33938393f9e7c6634e7d1e6459a3759
rongyi <sqrongyi@163.com>
2025-02-13 14:59:08 +0800

    Add 1386

--- src/1386.rs
+++ src/1386.rs
@@ -5,28 +5,32 @@ struct Solution;
 use std::collections::HashMap;
 impl Solution {
     pub fn max_number_of_families(n: i32, reserved_seats: Vec<Vec<i32>>) -> i32 {
-        let mut mask: HashMap<i32, i32> = HashMap::new();
+        let mut mask: HashMap<i32, Vec<i32>> = HashMap::new();
         for rsv in reserved_seats.iter() {
-            if let Some(&val) = mask.get(&rsv[0]) {
-                mask.insert(rsv[0], val | (1 << rsv[1]));
-            } else {
-                mask.insert(rsv[0], 1 << rsv[1]);
-            }
+            mask.entry(rsv[0]).or_default().push(rsv[1]);
         }
-        let mut ret = 0;
-        for (&_k, &cur_mask) in mask.iter() {
-            let mut cur_cnt = 0;
 
-            if ((15 << 2) & cur_mask) == 0 {
-                cur_cnt += 1;
+        let mut ret = 0;
+        for (&_k, cur_mask) in mask.iter() {
+            let mut blocks = vec![false; 3];
+
+            for &pos in cur_mask.iter() {
+                if pos >= 2 && pos <= 5 {
+                    blocks[0] = true;
+                }
+                if pos >= 6 && pos <= 9 {
+                    blocks[2] = true;
+                }
+                if pos >= 4 && pos <= 7 {
+                    blocks[1] = true;
+                }
             }
-            if ((15 << 6) & cur_mask) == 0 {
-                cur_cnt += 1;
+
+            if !blocks[0] && !blocks[2] {
+                ret += 2;
+            } else if blocks.iter().any(|&t| !t) {
+                ret += 1
             }
-            if cur_cnt == 0 && ((15 << 4) & cur_mask) == 0 {
-                cur_cnt += 1;
-            }
-            ret += cur_cnt;
         }
 
         ret + 2 * (n - mask.len() as i32)
