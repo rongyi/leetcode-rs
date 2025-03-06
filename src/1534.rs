958c9ceaa6f29a76ec8bda735130e3435091b34f
rongyi <sqrongyi@163.com>
2025-03-07 07:19:45 +0800

    Add 1534

---
+++ src/1534.rs
@@ -0,0 +1,25 @@
+#![allow(dead_code)]
+
+struct Solution;
+
+impl Solution {
+    pub fn count_good_triplets(arr: Vec<i32>, a: i32, b: i32, c: i32) -> i32 {
+        let n = arr.len();
+        let mut count = 0;
+
+        for i in 0..n {
+            for j in (i + 1)..n {
+                if (arr[i] - arr[j]).abs() <= a {
+                    for k in (j + 1)..n {
+                        if (arr[j] - arr[k]).abs() <= b && (arr[i] - arr[k]).abs() <= c {
+                            count += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        count
+    }
+}
+fn main() {}
