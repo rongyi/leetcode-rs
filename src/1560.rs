59a4981c69f39f6dbf5e0ae69985acc27cbc7493
rongyi <sqrongyi@163.com>
2025-03-10 11:49:42 +0800

    Add 1560

--- src/1560.rs
+++ src/1560.rs
@@ -9,6 +9,7 @@ impl Solution {
 
         let mut result = Vec::new();
 
+        // start --------- end
         if start <= end {
             // Simple case: when start <= end, the most visited sectors are just [start, end]
             for i in start..=end {
@@ -16,6 +17,7 @@ impl Solution {
             }
         } else {
             // Complex case: when start > end, the most visited sectors are [1, end] and [start, n]
+            // 1 ----- end ----- start ----- n
             for i in 1..=end {
                 result.push(i);
             }
