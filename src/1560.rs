9bc7d0ddfd46672178ed3c9d2096056e04f219dc
rongyi <sqrongyi@163.com>
2025-03-10 11:39:27 +0800

    Add 1560

---
+++ src/1560.rs
@@ -0,0 +1,31 @@
+#![allow(dead_code)]
+
+struct Solution;
+
+impl Solution {
+    pub fn most_visited(n: i32, rounds: Vec<i32>) -> Vec<i32> {
+        let start = rounds[0];
+        let end = *rounds.last().unwrap();
+
+        let mut result = Vec::new();
+
+        if start <= end {
+            // Simple case: when start <= end, the most visited sectors are just [start, end]
+            for i in start..=end {
+                result.push(i);
+            }
+        } else {
+            // Complex case: when start > end, the most visited sectors are [1, end] and [start, n]
+            for i in 1..=end {
+                result.push(i);
+            }
+            for i in start..=n {
+                result.push(i);
+            }
+        }
+
+        result
+    }
+}
+
+fn main() {}
