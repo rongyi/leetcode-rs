6d307595984554cd06123a8385ca80f0c883bd8c
rongyi <sqrongyi@163.com>
2025-03-12 11:55:31 +0800

    Add 1585

--- src/1585.rs
+++ src/1585.rs
@@ -2,47 +2,41 @@
 
 struct Solution;
 
+// Intuition
+// If ch[i] > ch[j], we can swap these characters. In other words, we can move a character freely to the left, until it hits a smaller character, e.g.:
+
+// "0231" > "0213" > "0123"
+
+// So, we do not need to sort anything, we just need to check if we can move required characters to the left to get the target string.
+
+// Note: we can also process the string right-to-left and move larger numbers right. In that case, we can just pop used indexes instead tracking them in a separate array pos. I though about it, but it appeared a bit harder to get right during the contest. If interested, check the solution by 0xFFFFFFFF in the comments below.
+
+// Algorithm
+// Collect indexes of all characters 0-9 of the source strings in idx. For each characters, we track which indexes we have used in pos.
+
+// For each character ch in the target string, check if we have it in idx. If so, verify that there are no smaller characters in front of it. To do that, we check the current idexes of all characters less than ch.
+
+// If the character can be moved, mark its index as used by advancing pos[ch].
+
 impl Solution {
     pub fn is_transformable(s: String, t: String) -> bool {
-        if s.len() != t.len() {
-            return false;
+        let mut idx: Vec<Vec<usize>> = vec![vec![]; 10];
+        let mut pos: Vec<usize> = vec![0; 10];
+        for (i, c) in s.chars().enumerate() {
+            idx[(c as u8 - b'0') as usize].push(i);
         }
-
-        let s_chars: Vec<char> = s.chars().collect();
-        let t_chars: Vec<char> = t.chars().collect();
-
-        // Create position arrays for each digit (0-9)
-        let mut positions: Vec<Vec<usize>> = vec![vec![]; 10];
-
-        // Store the positions of each digit in s
-        for (i, &ch) in s_chars.iter().enumerate() {
-            let digit = (ch as u8 - b'0') as usize;
-            positions[digit].push(i);
-        }
-
-        // Initialize index pointers for each digit's position list
-        let mut indices: Vec<usize> = vec![0; 10];
-
-        // Process each digit in t
-        for &ch in &t_chars {
-            let digit = (ch as u8 - b'0') as usize;
-
-            // Check if we've used all occurrences of this digit
-            if indices[digit] >= positions[digit].len() {
+        for c in t.chars() {
+            let d = (c as u8 - b'0') as usize;
+            if pos[d] >= idx[d].len() {
                 return false;
             }
 
-            let pos = positions[digit][indices[digit]];
-
-            // Check if there are smaller digits that come after this position
-            for d in 0..digit {
-                if indices[d] < positions[d].len() && positions[d][indices[d]] < pos {
+            for i in 0..d {
+                if pos[i] < idx[i].len() && idx[i][pos[i]] < idx[d][pos[d]] {
                     return false;
                 }
             }
-
-            // Mark this position as used
-            indices[digit] += 1;
+            pos[d] += 1;
         }
 
         true
