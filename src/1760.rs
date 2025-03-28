3d47cc5ead51e68fda0c1e33ad3dac81ab86cb88
rongyi <sqrongyi@163.com>
2025-03-28 17:24:41 +0800

    Add 1760

---
+++ src/1760.rs
@@ -0,0 +1,26 @@
+#![allow(dead_code)]
+
+struct Solution;
+
+impl Solution {
+    pub fn minimum_size(nums: Vec<i32>, max_operations: i32) -> i32 {
+        let mut left = 1;
+        let mut right = *nums.iter().max().unwrap();
+
+        while left < right {
+            let mid = left + (right - left) / 2;
+
+            let operations = nums.iter().map(|&num| (num - 1) / mid).sum::<i32>();
+
+            if operations <= max_operations {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        left
+    }
+}
+
+fn main() {}
