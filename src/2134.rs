856baaaded03c9c4b54881d51cf575eb50c00614
rongyi <sqrongyi@163.com>
2025-08-10 17:35:12 +0800

    Add 2134

---
+++ src/2134.rs
@@ -0,0 +1,24 @@
+struct Solution;
+
+impl Solution {
+    pub fn min_swaps(mut nums: Vec<i32>) -> i32 {
+        let ones = nums.iter().filter(|x| **x == 1).count();
+        // double it
+        nums.extend(nums.clone());
+        let mut ones_in_win = 0;
+        let mut cur_one_in_win = 0;
+        for (i, &val) in nums.iter().enumerate() {
+            if i >= ones && nums[i - ones] == 1 {
+                cur_one_in_win -= 1;
+            }
+            if val == 1 {
+                cur_one_in_win += 1;
+            }
+            ones_in_win = ones_in_win.max(cur_one_in_win);
+        }
+
+        (ones - ones_in_win) as _
+    }
+}
+
+fn main() {}
