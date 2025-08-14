07663b6a23cd8da76c98af2fa3d83f2dc356090f
rongyi <sqrongyi@163.com>
2025-08-15 07:45:04 +0800

    Add 2160

---
+++ src/2160.rs
@@ -0,0 +1,17 @@
+struct Solution;
+
+impl Solution {
+    pub fn minimum_sum(mut num: i32) -> i32 {
+        let mut digits: Vec<i32> = vec![];
+        while num != 0 {
+            digits.push(num % 10);
+            num /= 10;
+        }
+
+
+        digits.sort_unstable();
+        digits[0] * 10 + digits[1] * 10 + digits[2] + digits[3]
+    }
+}
+
+fn main() {}
