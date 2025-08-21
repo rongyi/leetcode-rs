8545bb710fedeba83a76ba1e57796e7506193293
rongyi <sqrongyi@163.com>
2025-08-21 08:02:36 +0800

    Add 2236

---
+++ src/2236.rs
@@ -0,0 +1,48 @@
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn check_tree(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        let root_val = root.as_ref().unwrap().borrow().val;
+        let left = root
+            .as_ref()
+            .unwrap()
+            .borrow()
+            .left
+            .as_ref()
+            .unwrap()
+            .borrow()
+            .val;
+        let right = root
+            .as_ref()
+            .unwrap()
+            .borrow()
+            .right
+            .as_ref()
+            .unwrap()
+            .borrow()
+            .val;
+        root_val == left + right
+    }
+}
+
+fn main() {}
