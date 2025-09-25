b2f633c1372df8aba49e2314a1eaf251f4a619b4
rongyi <sqrongyi@163.com>
2025-09-25 16:08:31 +0800

    Update 2433

--- src/2433.rs
+++ src/2433.rs
@@ -4,6 +4,10 @@ impl Solution {
     pub fn find_array(pref: Vec<i32>) -> Vec<i32> {
         let mut ret = vec![pref[0]];
 
+        // you can calculate the
+        // prefix ^: [a, b, c]
+        // origin: [a, x, y]
+        // x = a ^b, y = b^c
         for i in 1..pref.len() {
             ret.push(pref[i] ^ pref[i - 1]);
         }
