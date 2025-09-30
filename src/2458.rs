43ef6e9914599f0f679d1a543ef99df01ef50239
rongyi <sqrongyi@163.com>
2025-09-30 16:27:22 +0800

    Add 2458

---
+++ src/2458.rs
@@ -0,0 +1,115 @@
+struct Solution;
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn tree_queries(root: Option<Rc<RefCell<TreeNode>>>, queries: Vec<i32>) -> Vec<i32> {
+        // val's left child height, not includ it self
+        let mut left_heights: HashMap<i32, i32> = HashMap::new();
+        // val's right child height, not include it self
+        let mut right_heights: HashMap<i32, i32> = HashMap::new();
+        Self::edges(root.as_ref(), &mut left_heights, &mut right_heights);
+
+        let mut the_other_road: HashMap<i32, i32> = HashMap::new();
+        Self::peek_other_side(
+            root.as_ref().unwrap().borrow().left.as_ref(),
+            right_heights[&root.as_ref().unwrap().borrow().val],
+            1,
+            &mut the_other_road,
+            &left_heights,
+            &right_heights,
+        );
+        Self::peek_other_side(
+            root.as_ref().unwrap().borrow().right.as_ref(),
+            left_heights[&root.as_ref().unwrap().borrow().val],
+            1,
+            &mut the_other_road,
+            &left_heights,
+            &right_heights,
+        );
+        let mut ret = vec![];
+        for q in &queries {
+            ret.push(the_other_road[q]);
+        }
+
+        ret
+    }
+
+    fn peek_other_side(
+        node: Option<&Rc<RefCell<TreeNode>>>,
+        other_road_height: i32,
+        node_height: i32,
+        the_other_road: &mut HashMap<i32, i32>,
+        left_heights: &HashMap<i32, i32>,
+        right_heights: &HashMap<i32, i32>,
+    ) {
+        if let Some(node) = node {
+            let node = node.borrow();
+            the_other_road.insert(node.val, other_road_height);
+
+            Self::peek_other_side(
+                node.left.as_ref(),
+                other_road_height.max(node_height + right_heights[&node.val]),
+                node_height + 1,
+                the_other_road,
+                left_heights,
+                right_heights,
+            );
+
+            Self::peek_other_side(
+                node.right.as_ref(),
+                other_road_height.max(node_height + left_heights[&node.val]),
+                node_height + 1,
+                the_other_road,
+                left_heights,
+                right_heights,
+            );
+        }
+    }
+
+    // height means edge
+    fn edges(
+        node: Option<&Rc<RefCell<TreeNode>>>,
+        left_heights: &mut HashMap<i32, i32>,
+        right_heights: &mut HashMap<i32, i32>,
+    ) -> i32 {
+        if let Some(node) = node {
+            let n = node.borrow();
+            let val = n.val;
+
+            let lh = Self::edges(n.left.as_ref(), left_heights, right_heights);
+            let rh = Self::edges(n.right.as_ref(), left_heights, right_heights);
+
+            // cur's left height is
+            left_heights.insert(val, lh);
+            // cur's right height is
+            right_heights.insert(val, rh);
+
+            lh.max(rh) + 1
+        } else {
+            0
+        }
+    }
+}
+fn main() {}
