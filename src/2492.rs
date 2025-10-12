6d9611b1c7cdb321ec36d41b5cc152e44d9bf8dd
rongyi <sqrongyi@163.com>
2025-10-12 10:07:45 +0800

    Add 2492

--- src/2492.rs
+++ src/2492.rs
@@ -1,6 +1,6 @@
 struct Solution;
 
-use std::i32;
+use std::{collections::VecDeque, i32};
 impl Solution {
     pub fn min_score(n: i32, roads: Vec<Vec<i32>>) -> i32 {
         let mut graph: Vec<Vec<(usize, i32)>> = vec![vec![]; n as usize + 1];
@@ -12,16 +12,31 @@ impl Solution {
         }
         let mut visited = vec![false; n as usize + 1];
 
-        Self::dfs(1, &graph, &mut visited)
+        let mut q: VecDeque<(usize, i32)> = VecDeque::new();
+        q.push_back((1, i32::MAX));
+        let mut ret = i32::MAX;
+        while let Some((cur, weight)) = q.pop_front() {
+            visited[cur] = true;
+            ret = ret.min(weight);
+            for &(next, next_weight) in graph[cur].iter() {
+                if visited[next] {
+                    continue;
+                }
+                q.push_back((next, ret.min(next_weight)));
+            }
+        }
+
+        ret
     }
     fn dfs(cur: usize, graph: &Vec<Vec<(usize, i32)>>, visited: &mut Vec<bool>) -> i32 {
         let mut ret = i32::MAX;
         visited[cur] = true;
         for &(next, weight) in graph[cur].iter() {
-            if !visited[next] {
-                ret = ret.min(Self::dfs(next, graph, visited));
-            }
             ret = ret.min(weight);
+            if visited[next] {
+                continue;
+            }
+            ret = ret.min(Self::dfs(next, graph, visited));
         }
         ret
     }
