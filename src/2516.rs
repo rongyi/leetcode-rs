a02be2770ba9ebdf4d509491a9a397d9a11dd0a6
rongyi <sqrongyi@163.com>
2025-10-14 17:53:21 +0800

    Add 2516

---
+++ src/2516.rs
@@ -0,0 +1,34 @@
+struct Solution;
+
+impl Solution {
+    pub fn take_characters(s: String, k: i32) -> i32 {
+        let s: Vec<usize> = s.chars().map(|c| (c as u8 - 'a' as u8) as usize).collect();
+        let sz = s.len();
+        let mut acc = vec![0; 3];
+        for &v in s.iter() {
+            acc[v] += 1;
+        }
+        if acc.iter().any(|&c| c < k) {
+            return -1;
+        }
+        let mut i = 0;
+        let mut win_sz = 0;
+        let mut ret = sz;
+        // in win range we consume one, and left is decresing
+        for j in 0..sz {
+            acc[s[j]] -= 1;
+            win_sz += 1;
+            while acc[s[j]] < k {
+                acc[s[i]] += 1;
+                i += 1;
+
+                win_sz -= 1;
+            }
+            ret = ret.min(sz - win_sz);
+        }
+
+        ret as _
+    }
+}
+
+fn main() {}
