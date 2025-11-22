63c2821e4db9a91e07506653b2eb50547da54f62
rongyi <sqrongyi@163.com>
2025-11-22 11:48:16 +0800

    Add 2461

--- src/2461.rs
+++ src/2461.rs
@@ -22,39 +22,40 @@ use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
     pub fn replace_value_in_tree(
-        root: Option<Rc<RefCell<TreeNode>>>,
+        mut root: Option<Rc<RefCell<TreeNode>>>,
     ) -> Option<Rc<RefCell<TreeNode>>> {
-        let root = if let Some(root) = root {
-            root
-        } else {
-            return None;
-        };
-        let mut level_sum = 0;
-        let mut q = vec![(root.clone(), 0)];
-        while !q.is_empty() {
-            let mut new_queue = vec![];
-            let mut new_level_sum = 0;
-            for (node, subtree_sum) in q.into_iter() {
-                node.borrow_mut().val = level_sum - subtree_sum;
-                let mut subtree_sum = 0;
-                let mut children = vec![];
-                if let Some(l) = node.borrow().left.clone() {
-                    subtree_sum += l.borrow().val;
-                    children.push(l);
+        match root {
+            None => None,
+            Some(root) => {
+                let mut level_sum = 0;
+                let mut q = vec![(root.clone(), 0)];
+                while !q.is_empty() {
+                    let mut new_layer = vec![];
+                    let mut new_layer_sum = 0;
+                    for (node, subtree_sum) in q.into_iter() {
+                        node.borrow_mut().val = level_sum - subtree_sum;
+                        let mut child = vec![];
+                        let mut cur_subtree_sum = 0;
+                        if let Some(l) = node.borrow().left.clone() {
+                            cur_subtree_sum += l.borrow().val;
+                            child.push(l);
+                        }
+                        if let Some(r) = node.borrow().right.clone() {
+                            cur_subtree_sum += r.borrow().val;
+                            child.push(r);
+                        }
+                        child
+                            .into_iter()
+                            .for_each(|node| new_layer.push((node, cur_subtree_sum)));
+                        new_layer_sum += cur_subtree_sum;
+                    }
+                    level_sum = new_layer_sum;
+                    q = new_layer;
                 }
-                if let Some(r) = node.borrow().right.clone() {
-                    subtree_sum += r.borrow().val;
-                    children.push(r);
-                }
-                new_level_sum += subtree_sum;
-                children
-                    .into_iter()
-                    .for_each(|node| new_queue.push((node, subtree_sum)));
+
+                Some(root.clone())
             }
-            q = new_queue;
-            level_sum = new_level_sum;
         }
-        Some(root)
     }
 }
 
