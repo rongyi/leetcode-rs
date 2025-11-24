ffa5cfbb2327224fc852d2eaf782cb64f0304539
rongyi <sqrongyi@163.com>
2025-11-24 16:55:30 +0800

    Add 2652

---
+++ src/2652.rs
@@ -0,0 +1,11 @@
+struct Solution;
+
+impl Solution {
+    pub fn sum_of_multiples(n: i32) -> i32 {
+        (1..=n)
+            .filter(|&num| num % 3 == 0 || num % 5 == 0 || num % 7 == 0)
+            .sum()
+    }
+}
+
+fn main() {}
