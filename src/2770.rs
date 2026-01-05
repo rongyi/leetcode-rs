688938598435257ef22541016648465ce7c6d370
rongyi <sqrongyi@163.com>
2026-01-05 16:46:13 +0800

    Update

--- src/2770.rs
+++ src/2770.rs
@@ -3,13 +3,13 @@ struct Solution;
 impl Solution {
     pub fn maximum_jumps(nums: Vec<i32>, target: i32) -> i32 {
         let sz = nums.len();
-        let mut dp: Vec<i32> = vec![i32::MIN; sz];
+        let mut dp: Vec<i32> = vec![-1; sz];
         // already on index 0
         dp[0] = 0;
 
         for i in 0..sz {
             // you can not step on this index
-            if dp[i] == i32::MIN {
+            if dp[i] == -1 {
                 continue;
             }
             // let mut wayout = false;
@@ -20,15 +20,8 @@ impl Solution {
             }
         }
 
-        if dp[sz - 1] == i32::MIN {
-            return -1;
-        }
         dp[sz - 1]
     }
 }
 
-fn main() {
-    let input = vec![0, 2, 1, 3];
-    let val = Solution::maximum_jumps(input, 1);
-    println!("{val}");
-}
+fn main() {}
