35916ef8fb38048a9b4ba956cf8b97d1a421396e
rongyi <sqrongyi@163.com>
2026-01-15 16:00:31 +0800

    Add 2787

---
+++ src/2787.rs
@@ -0,0 +1,28 @@
+struct Solution;
+
+impl Solution {
+    pub fn number_of_ways(n: i32, k: i32) -> i32 {
+        let n = n as usize;
+        let m = 1_000_000_007;
+
+        let mut dp = vec![0; n + 1];
+        dp[0] = 1;
+
+        let mut i: i32 = 1;
+        let k = k as u32;
+        loop {
+            let p = i.pow(k) as usize;
+            if p > n {
+                break;
+            }
+            for s in (p..=n).rev() {
+                dp[s] = (dp[s] + dp[s - p]) % m;
+            }
+            i += 1;
+        }
+
+        dp[n]
+    }
+}
+
+fn main() {}
