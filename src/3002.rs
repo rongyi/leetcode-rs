1fda0c01bc7035fc43e8ba867f86d397602e042c
rongyi <sqrongyi@163.com>
2026-03-17 07:50:33 +0800

    Add 3002

--- src/3002.rs
+++ src/3002.rs
@@ -1,27 +1,37 @@
 struct Solution;
+use std::cmp::min;
+use std::collections::HashSet;
 
 impl Solution {
     pub fn maximum_set_size(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
-        use std::collections::{HashMap, HashSet};
-
         let n = nums1.len();
-        let target = n / 2;
+        let limit = n / 2;
 
-        let set1: HashSet<_> = nums1.into_iter().collect();
-        let set2: HashSet<_> = nums2.into_iter().collect();
+        let set1: HashSet<i32> = nums1.into_iter().collect();
+        let set2: HashSet<i32> = nums2.into_iter().collect();
 
-        let common: HashSet<_> = set1.intersection(&set2).cloned().collect();
+        // Elements unique to set1
+        let unique1 = set1.iter().filter(|x| !set2.contains(x)).count();
+        // Elements unique to set2
+        let unique2 = set2.iter().filter(|x| !set1.contains(x)).count();
+        // Elements present in both
+        let common = set1.len() - unique1;
 
-        let only1: HashSet<_> = set1.difference(&common).cloned().collect();
-        let only2: HashSet<_> = set2.difference(&common).cloned().collect();
+        // How many unique elements can we contribute from each side?
+        // We can't take more than n/2 from either side.
+        let take1 = min(unique1, limit);
+        let take2 = min(unique2, limit);
 
-        let take_only1 = only1.len().min(target);
-        let take_only2 = only2.len().min(target);
+        // After taking unique elements, how much "room" is left in our n/2 quotas?
+        let room1 = limit - take1;
+        let room2 = limit - take2;
 
-        let remaining = target - take_only1;
-        let take_common = common.len().min(remaining + (target - take_only2));
+        // We can fill the remaining room with common elements.
+        // The total unique elements will be:
+        // (elements from set1) + (elements from set2) + (common elements we can fit)
+        let total_possible = take1 + take2 + min(common, room1 + room2);
 
-        (take_only1 + take_only2 + take_common) as i32
+        total_possible as i32
     }
 }
 
