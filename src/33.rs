d671e31e1117ddb575b5b86217363994be06884d
rongyi <sqrongyi@163.com>
2026-05-12 17:39:58 +0800

    Add 33

--- src/33.rs
+++ src/33.rs
@@ -2,24 +2,27 @@ struct Solution;
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let mut left = 0;
+        // one half must be sorted
+        let mut left = 0 as i32;
         let mut right = nums.len() as i32 - 1;
+
         while left <= right {
             let mid = left + (right - left) / 2;
             let mid_val = nums[mid as usize];
-            if mid_val == target {
+            if target == mid_val {
                 return mid;
             }
+            // check which side is sorted
             if nums[left as usize] <= mid_val {
-                // left half is sorted
+                // left chunk is sorted
                 if target >= nums[left as usize] && target < mid_val {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
             } else {
-                // right half is sorted
-                if target > mid_val && target <= nums[right as usize] {
+                // right chunk is sorted
+                if target <= nums[right as usize] && target > mid_val {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
