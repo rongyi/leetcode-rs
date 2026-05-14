a745cbec2f2c625dacfa993b2da55b47556470f7
rongyi <sqrongyi@163.com>
2026-05-14 11:10:43 +0800

    Update 41

--- src/41.rs
+++ src/41.rs
@@ -2,22 +2,24 @@ struct Solution;
 
 impl Solution {
     pub fn first_missing_positive(mut nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-
-        for i in 0..n {
-            while nums[i] > 0 && nums[i] <= n as i32 && nums[nums[i] as usize - 1] != nums[i] {
+        // index:  0, 1, 2, 3, 4
+        // expect: 1, 2, 3, 4, 5
+        // bucket sort, like pegeon hole
+        let sz = nums.len();
+        for i in 0..sz {
+            while nums[i] > 0 && nums[i] <= sz as i32 && nums[nums[i] as usize - 1] != nums[i] {
                 let target_idx = nums[i] as usize - 1;
                 nums.swap(target_idx, i);
             }
         }
 
-        for i in 0..n {
-            if nums[i] != i as i32 + 1 {
-                return (i + 1) as i32;
+        for i in 0..sz {
+            if nums[i] != (i + 1) as i32 {
+                return (i + 1) as _;
             }
         }
 
-        (n + 1) as i32
+        (sz + 1) as _
     }
 }
 
