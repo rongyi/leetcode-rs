60d1ca4c59cc21ed5c42f6bfcbae1a99b38b7fa8
rongyi <sqrongyi@163.com>
2026-05-15 16:57:51 +0800

    Update 51

--- src/51.rs
+++ src/51.rs
@@ -12,7 +12,7 @@ impl Solution {
         let mut diag2 = HashMap::new();
 
         Self::dfs(
-            &mut cur, 0, 0, sz, &mut rows, &mut cols, &mut diag1, &mut diag2, n, &mut out,
+            &mut cur, 0, sz, &mut rows, &mut cols, &mut diag1, &mut diag2, &mut out,
         );
 
         out
@@ -20,61 +20,38 @@ impl Solution {
     fn dfs(
         cur: &mut Vec<Vec<char>>,
         x: usize,
-        y: usize,
         sz: usize,
         rows: &mut Vec<bool>,
         cols: &mut Vec<bool>,
         diag1: &mut HashMap<i32, bool>,
         diag2: &mut HashMap<i32, bool>,
-        queue_left: i32,
         out: &mut Vec<Vec<String>>,
     ) {
         if x == sz {
             // ok, a valid
-            if queue_left == 0 {
-                let cur_s: Vec<String> =
-                    cur.iter().map(|vc| vc.iter().copied().collect()).collect();
-                out.push(cur_s);
-            }
+            let cur_s: Vec<String> = cur.iter().map(|vc| vc.iter().copied().collect()).collect();
+            out.push(cur_s);
             return;
         }
-        if y == sz {
-            return Self::dfs(cur, x + 1, 0, sz, rows, cols, diag1, diag2, queue_left, out);
-        }
-        // 1. first I can put '.' in this pos, right?
-        // because init with '.' so no put action
-        Self::dfs(cur, x, y + 1, sz, rows, cols, diag1, diag2, queue_left, out);
-        // 2. we put 'Q' in this position but only with valid case
-        if queue_left > 0
-            && !rows[x]
-            && !cols[y]
-            && !diag1.get(&((x + y) as i32)).unwrap_or(&false)
-            && !diag2.get(&(x as i32 - y as i32)).unwrap_or(&false)
-        {
-            cur[x][y] = 'Q';
-            rows[x] = true;
-            cols[y] = true;
-            diag1.insert(x as i32 + y as i32, true);
-            diag2.insert(x as i32 - y as i32, true);
 
-            Self::dfs(
-                cur,
-                x,
-                y + 1,
-                sz,
-                rows,
-                cols,
-                diag1,
-                diag2,
-                queue_left - 1,
-                out,
-            );
+        for j in 0..sz {
+            let k1 = x as i32 + j as i32;
+            let k2 = x as i32 - j as i32;
+            if cols[j] || *diag1.get(&k1).unwrap_or(&false) || *diag2.get(&k2).unwrap_or(&false) {
+                continue;
+            }
 
-            cur[x][y] = '.';
-            rows[x] = false;
-            cols[y] = false;
-            diag1.remove(&(x as i32 + y as i32));
-            diag2.remove(&(x as i32 - y as i32));
+            cols[j] = true;
+            diag1.insert(k1, true);
+            diag2.insert(k2, true);
+            cur[x][j] = 'Q';
+
+            Self::dfs(cur, x + 1, sz, rows, cols, diag1, diag2, out);
+
+            cur[x][j] = '.';
+            cols[j] = false;
+            diag1.remove(&k1);
+            diag2.remove(&k2);
         }
     }
 }
