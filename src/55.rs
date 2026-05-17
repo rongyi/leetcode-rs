f93e020b7fb40219580b57bc5d388981f897c7a0
rongyi <sqrongyi@163.com>
2026-05-17 10:58:49 +0800

    Add 55

---
+++ src/55.rs
@@ -0,0 +1,21 @@
+struct Solution;
+
+impl Solution {
+    pub fn can_jump(nums: Vec<i32>) -> bool {
+        let mut cur_pos = 0;
+        let mut far_reach = 0;
+        for (i, &step) in nums.iter().enumerate() {
+            if i > far_reach {
+                break;
+            }
+            far_reach = far_reach.max(i + step as usize);
+
+            if far_reach >= nums.len() - 1 {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+fn main() {}
