c02b37e296160582f06cd3f3ee68313f0c85d60d
rongyi <sqrongyi@163.com>
2026-06-04 09:30:21 +0800

    Add 154

---
+++ src/154.rs
@@ -0,0 +1,23 @@
+struct Solution;
+
+impl Solution {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+        while left < right {
+            let mid = left + (right - left) / 2;
+            // normal range, go left
+            if nums[mid] < nums[right] {
+                right = mid;
+            } else if nums[mid] > nums[right] {
+                left = mid + 1;
+            } else {
+                right -= 1;
+            }
+        }
+
+        nums[left]
+    }
+}
+
+fn main() {}
