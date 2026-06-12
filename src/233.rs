12ad5139248f718633b846ebf2cdcab36006ca31
rongyi <sqrongyi@163.com>
2026-06-12 17:09:37 +0800

    Add 233

---
+++ src/233.rs
@@ -0,0 +1,64 @@
+struct Solution;
+
+impl Solution {
+    // For people who doesn't understand the author's explanations, just look at some examples:
+    //
+    // Let n = 4560000
+    //
+    // How many nums with "1" at the thousand's position?
+    //
+    // 4551000 to 4551999 (1000)
+    // 4541000 to 4541999 (1000)
+    // 4531000 to 4531999 (1000)
+    // ...
+    // 1000 to 1999 (1000)
+    //
+    // That's 456 * 1000
+    //
+    // What if n = 4561234?
+    //
+    // 4561000-4561234 (1234+1)
+    // 4551000 to 4551999 (1000)
+    // 4541000 to 4541999 (1000)
+    // 4531000 to 4531999 (1000)
+    // ...
+    // 1000 to 1999 (1000)
+    //
+    // That's 456 * 1000 + 1234 + 1
+    //
+    // What if n = 4562345?
+    // 4561000-4561999 (1000)
+    // 4551000 to 4551999 (1000)
+    // 4541000 to 4541999 (1000)
+    // 4531000 to 4531999 (1000)
+    // ...
+    // 1000 to 1999 (1000)
+    //
+    // That's 456*1000 + 1000
+
+    pub fn count_digit_one(n: i32) -> i32 {
+        let mut count = 0;
+        let mut factor = 1;
+        let n = n as i64;
+        let mut cur = n / factor;
+
+        while cur > 0 {
+            let r = cur % 10;
+            let q = cur / 10;
+
+            count += q * factor;
+
+            if r == 1 {
+                count += n % factor + 1;
+            } else if r > 1 {
+                count += factor;
+            }
+
+            factor *= 10;
+            cur = n / factor;
+        }
+        count as i32
+    }
+}
+
+fn main() {}
