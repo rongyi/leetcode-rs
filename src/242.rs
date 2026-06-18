bd6babb84cf1b8a63965f7716839d24cf65a893d
rongyi <sqrongyi@163.com>
2026-06-18 17:19:02 +0800

    Add 242

---
+++ src/242.rs
@@ -0,0 +1,14 @@
+struct Solution;
+
+impl Solution {
+    pub fn is_anagram(s: String, t: String) -> bool {
+        let mut s: Vec<char> = s.chars().collect();
+        let mut t: Vec<char> = t.chars().collect();
+        s.sort_unstable();
+        t.sort_unstable();
+
+        s == t
+    }
+}
+
+fn main() {}
