3a2248053a2c80be42a468e4e5a7c2affe06ec16
rongyi <sqrongyi@163.com>
2026-06-22 10:43:31 +0800

    add 263

---
+++ src/263.rs
@@ -0,0 +1,25 @@
+struct Solution;
+
+impl Solution {
+    pub fn is_ugly(mut n: i32) -> bool {
+        if n <= 0 {
+            return false;
+        }
+
+        while n % 2 == 0 {
+            n /= 2;
+        }
+
+        while n % 3 == 0 {
+            n /= 3;
+        }
+
+        while n % 5 == 0 {
+            n /= 5;
+        }
+
+        n == 1
+    }
+}
+
+fn main() {}
