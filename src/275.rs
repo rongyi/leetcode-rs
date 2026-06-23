95bd749bfd9066e31cadb9f1f3a1834e25d53f79
rongyi <sqrongyi@163.com>
2026-06-23 15:52:27 +0800

    Add 275

---
+++ src/275.rs
@@ -0,0 +1,27 @@
+struct Solution;
+
+impl Solution {
+    pub fn h_index(citations: Vec<i32>) -> i32 {
+        let sz = citations.len() as i32;
+        let mut l: i32 = 0;
+        let mut r: i32 = sz - 1;
+        while l <= r {
+            let mid = l + (r - l) / 2;
+            let paper_cnt = sz - mid;
+
+            if paper_cnt == citations[mid as usize] {
+                return paper_cnt;
+            } else if paper_cnt < citations[mid as usize] {
+                // add apaper, move left
+                r = mid - 1;
+            } else {
+                // shrink paper, move right
+                l = mid + 1;
+            }
+        }
+
+        sz - (r + 1)
+    }
+}
+
+fn main() {}
