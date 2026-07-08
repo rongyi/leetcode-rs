3fc1f8e0787e80614bcad8bed32f95ef71228ce3
rongyi <sqrongyi@163.com>
2026-07-08 11:15:01 +0800

    Add 413

---
+++ src/413.rs
@@ -0,0 +1,33 @@
+struct Solution;
+
+impl Solution {
+    pub fn number_of_arithmetic_slices(nums: Vec<i32>) -> i32 {
+        let sz = nums.len();
+        if sz < 3 {
+            return 0;
+        }
+        let mut ret = 0;
+
+        for i in 0..=sz - 2 {
+            for j in i + 2..sz {
+                if Self::is_arithmetic(&nums, i, j) {
+                    ret += 1;
+                }
+            }
+        }
+
+        ret
+    }
+
+    fn is_arithmetic(nums: &[i32], i: usize, j: usize) -> bool {
+        let delta = nums[i + 1] - nums[i];
+        for k in i + 2..=j {
+            if nums[k] - nums[k - 1] != delta {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+fn main() {}
