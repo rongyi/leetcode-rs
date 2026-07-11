22da77dd2392157501bf8f384539640d19a68618
rongyi <sqrongyi@163.com>
2026-07-11 18:29:55 +0800

    Add 455

---
+++ src/455.rs
@@ -0,0 +1,24 @@
+struct Solution;
+
+impl Solution {
+    pub fn find_content_children(mut g: Vec<i32>, mut s: Vec<i32>) -> i32 {
+        g.sort();
+        s.sort();
+        let mut i = 0;
+        let mut j = 0;
+        let mut cnt = 0;
+        while i < g.len() && j < s.len() {
+            if g[i] <= s[j] {
+                cnt += 1;
+                i += 1;
+            }
+            // 1. consumed, we shift to next
+            // 2. not consumed, we shift to next to give g[i] another try
+            j += 1;
+        }
+
+        cnt
+    }
+}
+
+fn main() {}
