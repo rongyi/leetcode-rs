5f6997b36d9a805c73380a16432c56d2b31cb73f
rongyi <sqrongyi@163.com>
2026-07-21 15:49:35 +0800

    Add 523

--- src/523.rs
+++ src/523.rs
@@ -5,18 +5,22 @@ impl Solution {
     pub fn check_subarray_sum(nums: Vec<i32>, k: i32) -> bool {
         let mut sum = 0;
         let mut modk: HashMap<i32, i32> = HashMap::new();
+
         modk.insert(0, -1);
         for (i, &num) in nums.iter().enumerate() {
             let i = i as i32;
             sum += num;
             sum %= k;
 
-            if modk.contains_key(&sum) {
-                if i - modk[&sum] > 1 {
-                    return true;
+            match modk.get(&sum) {
+                Some(&prev) => {
+                    if i - prev > 1 {
+                        return true;
+                    }
                 }
-            } else {
-                modk.insert(sum, i);
+                None => {
+                    modk.insert(sum, i);
+                }
             }
         }
         false
