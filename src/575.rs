0ae386410a55b9dad821c7ee8ab8919c5cf6d75d
rongyi <sqrongyi@163.com>
2026-07-27 14:35:13 +0800

    Update 575

--- src/575.rs
+++ src/575.rs
@@ -3,7 +3,7 @@ struct Solution;
 use std::collections::HashSet;
 impl Solution {
     pub fn distribute_candies(candy_type: Vec<i32>) -> i32 {
-        let uniq: HashSet<i32> = candy_type.iter().cloned().collect();
+        let uniq: HashSet<i32> = candy_type.iter().copied().collect();
 
         uniq.len().min(candy_type.len() / 2) as i32
     }
