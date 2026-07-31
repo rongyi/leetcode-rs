5bffafe423fed738b7e1c6c44f6e94e3ea15eb65
rongyi <sqrongyi@163.com>
2026-07-31 17:23:04 +0800

    Add 630

---
+++ src/630.rs
@@ -0,0 +1,21 @@
+struct Solution;
+
+use std::collections::BinaryHeap;
+impl Solution {
+    pub fn schedule_course(mut courses: Vec<Vec<i32>>) -> i32 {
+        courses.sort_by(|l, r| l[1].cmp(&r[1]));
+        let mut pq: BinaryHeap<i32> = BinaryHeap::new();
+        let mut sum = 0;
+        for c in courses.iter() {
+            pq.push(c[0]);
+            sum += c[0];
+            if sum > c[1] {
+                // longest duration got out
+                sum -= pq.pop().unwrap();
+            }
+        }
+        pq.len() as i32
+    }
+}
+
+fn main() {}
