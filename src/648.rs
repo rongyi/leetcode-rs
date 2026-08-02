c0486760819f331fdbcd41a07516c9b8b11f459e
rongyi <sqrongyi@163.com>
2026-08-02 10:34:27 +0800

    Add 648

--- src/648.rs
+++ src/648.rs
@@ -2,7 +2,8 @@ struct Solution;
 
 impl Solution {
     pub fn replace_words(mut dictionary: Vec<String>, sentence: String) -> String {
-        dictionary.sort_by(|l, r| l.len().cmp(&r.len()));
+        dictionary.sort_by_key(|v| v.len());
+
         let mut converts: Vec<String> = Vec::new();
         for word in sentence.split(' ') {
             let mut changed = false;
