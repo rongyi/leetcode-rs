384fd392da42180d587045fa6b4390521187b7fe
rongyi <sqrongyi@163.com>
2026-08-08 10:29:03 +0800

    Update 686, prev now can not pass all testcase

--- src/686.rs
+++ src/686.rs
@@ -1,23 +1,25 @@
-#![allow(dead_code)]
 struct Solution;
 
 impl Solution {
     pub fn repeated_string_match(a: String, b: String) -> i32 {
         let mut acp = a.clone();
         let mut ret = 1;
-        // 1. when b is so small, that we only need double a, if we can not find b in doubled a thats it
-        // 2. when b.len > a.len, we need this time to let a's len catch up b len
-        // then we double it to search that
-        // so both case we get condition:
-        while ret <= (b.len() / a.len()) as i32 + 2 {
+        loop {
             if acp.find(&b).is_some() {
                 return ret;
             }
 
             ret += 1;
             acp.push_str(&a);
+
+            if acp.len() >= b.len() * 2 {
+                break;
+            }
         }
 
+        if acp.find(&b).is_some() {
+            return ret;
+        }
         -1
     }
 }
