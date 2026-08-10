a5622bd69fd469e61abf0002a36889af4281cb2a
rongyi <sqrongyi@163.com>
2026-08-10 11:11:46 +0800

    Update 701

--- src/701.rs
+++ src/701.rs
@@ -1,25 +1,4 @@
-#![allow(dead_code)]
-
 struct Solution;
-
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
 
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -36,6 +15,7 @@ impl Solution {
 
         root
     }
+
     fn do_insert(mut root: Option<Rc<RefCell<TreeNode>>>, new_node: TreeNode) {
         if let Some(node) = root {
             let mut node = node.borrow_mut();
