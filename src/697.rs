097773e22aae7022d0a891f8f3a65a869a65c89a
rongyi <sqrongyi@163.com>
2026-08-10 10:55:00 +0800

    Update 697

--- src/697.rs
+++ src/697.rs
@@ -1,50 +1,34 @@
-#![allow(dead_code)]
-
 struct Solution;
 
 use std::collections::HashMap;
 impl Solution {
     pub fn find_shortest_sub_array(nums: Vec<i32>) -> i32 {
-        let mut cnt: HashMap<i32, usize> = HashMap::new();
-        for &num in nums.iter() {
-            *cnt.entry(num).or_insert(0) += 1;
-        }
-        let mut max_freq = 0;
-        for &v in cnt.values() {
-            max_freq = max_freq.max(v);
-        }
-        let mut max_nums = Vec::new();
-        for (&k, &v) in cnt.iter() {
-            if v == max_freq {
-                max_nums.push(k);
-            }
-        }
-        let mut ret = nums.len();
+        // Map stores: num -> (count, first_index, last_index)
+        let mut map: HashMap<i32, (usize, usize, usize)> = HashMap::new();
+        let mut max_degree = 0;
 
-        for &num in max_nums.iter() {
-            let mut start = 0;
-            for i in 0..nums.len() {
-                if nums[i] == num {
-                    start = i;
-                    break;
-                }
-            }
-            let mut end = nums.len() - 1;
-            for j in (0..nums.len()).rev() {
-                if nums[j] == num {
-                    end = j;
-                    break;
-                }
-            }
-            ret = ret.min(end - start + 1);
+        for (i, &num) in nums.iter().enumerate() {
+            let entry = map
+                .entry(num)
+                .and_modify(|e| {
+                    e.0 += 1;
+                    e.2 = i;
+                })
+                .or_insert((1, i, i));
+
+            max_degree = max_degree.max(entry.0);
         }
 
-        ret as i32
+        let mut min_len = nums.len();
+
+        for (count, first, last) in map.values() {
+            if *count == max_degree {
+                min_len = min_len.min(last - first + 1);
+            }
+        }
+
+        min_len as i32
     }
 }
 
-fn main() {
-    let input = [1, 2, 2, 3, 1].into_iter().collect();
-    let val = Solution::find_shortest_sub_array(input);
-    println!("{}", val);
-}
+fn main() {}
