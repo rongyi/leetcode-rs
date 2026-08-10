af53d879ca3d06d604740c5c3808b0e4dc063590
rongyi <sqrongyi@163.com>
2026-08-10 15:50:41 +0800

    Add 719

--- src/719.rs
+++ src/719.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 struct Solution;
 
 impl Solution {
@@ -7,13 +5,14 @@ impl Solution {
         nums.sort_unstable();
         let sz = nums.len();
         let mut left = 0;
-        let mut right = *nums.last().unwrap() - nums[0];
+        let mut right = nums[sz - 1] - nums[0];
 
         // 意思是让我们来假设当前第K个值为mid，然后我们来数一数前面有多少个数呢？
         // 如果数下来恰好是第K个，那么就对了
         // 多了，那么说明真正第K个元素比我们预估的要小(我们估大发了)，那么预估的mid要往小的方向走
         // 少了，说明真正第K个元素比我们预估的要大， 预估的值(mid)往大了走
         while left < right {
+            // mid is distance
             let mid = left + (right - left) / 2;
             let mut cnt = 0;
             let mut start = 0;
@@ -21,7 +20,6 @@ impl Solution {
                 while start < sz && (nums[i] - nums[start] > mid) {
                     start += 1;
                 }
-
                 cnt += i - start;
             }
             if cnt < k as usize {
@@ -30,6 +28,7 @@ impl Solution {
                 right = mid;
             }
         }
+
         right
     }
 }
