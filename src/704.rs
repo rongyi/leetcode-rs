5386c57508c5def31741f2e39b79550f832fe7fa
rongyi <sqrongyi@163.com>
2026-08-10 11:26:10 +0800

    Update 704

--- src/704.rs
+++ src/704.rs
@@ -1,18 +1,18 @@
-#![allow(dead_code)]
 struct Solution;
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let mut l = 0;
-        let mut r = nums.len();
-        while l < r {
+        let mut l = 0i32;
+        let mut r = nums.len() as i32 - 1;
+
+        while l <= r {
             let mid = l + (r - l) / 2;
-            if target == nums[mid] {
+            if target == nums[mid as usize] {
                 return mid as i32;
-            } else if target > nums[mid] {
+            } else if target > nums[mid as usize] {
                 l = mid + 1;
             } else {
-                r = mid;
+                r = mid - 1;
             }
         }
 
