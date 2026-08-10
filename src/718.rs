1e2c8d8d4035480311c5bc6c3a30704eea2083a8
rongyi <sqrongyi@163.com>
2026-08-10 15:20:56 +0800

    Update 718

--- src/718.rs
+++ src/718.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 struct Solution;
 
 impl Solution {
@@ -8,14 +6,12 @@ impl Solution {
         let n = b.len();
         let mut dp = vec![vec![0; n + 1]; m + 1];
         let mut ret = 0;
-        for i in (0..m).rev() {
-            for j in (0..n).rev() {
-                dp[i][j] = if a[i] == b[j] {
-                    1 + dp[i + 1][j + 1]
-                } else {
-                    0
-                };
-                ret = ret.max(dp[i][j]);
+        for i in 0..m {
+            for j in 0..n {
+                if a[i] == b[j] {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    ret = ret.max(dp[i + 1][j + 1]);
+                }
             }
         }
 
