7225dca1fc623f341cf6b288229d61344f25e505
rongyi <sqrongyi@163.com>
2026-08-11 10:42:58 +0800

    Update 726

--- src/726.rs
+++ src/726.rs
@@ -1,39 +1,33 @@
-#![allow(dead_code)]
-
-
 struct Solution;
 
 use std::collections::BTreeMap;
 impl Solution {
     pub fn count_of_atoms(formula: String) -> String {
-        let s = formula.chars().collect::<Vec<_>>();
+        let s = formula.as_bytes();
         let sz = s.len();
+        let mut i = 0;
 
         let mut stack: Vec<BTreeMap<String, i32>> = Vec::new();
-        // global env
+        // just like global env
         stack.push(BTreeMap::new());
 
-        let mut i = 0;
         while i < sz {
-            // TODO: update i
             match s[i] {
-                '(' => {
-                    // simply add a new env entry to stack
+                b'(' => {
+                    // sub block env
                     stack.push(BTreeMap::new());
                     i += 1;
                 }
-                ')' => {
+                b')' => {
                     let cur = stack.pop().unwrap();
+                    // merge cur to parent
                     let parent = stack.last_mut().unwrap();
-                    // TODO: add cur env to parent
-                    // find multiply value e.g. (H2O)
                     let mut j = i + 1;
                     while j < sz && s[j].is_ascii_digit() {
                         j += 1;
                     }
-                    let num_str: String = s[i + 1..j].iter().collect();
-                    let val = num_str.parse::<i32>().unwrap_or(1);
-
+                    let val = formula[i + 1..j].parse::<i32>().unwrap_or(1);
+                    // merge
                     for (k, v) in cur.into_iter() {
                         *parent.entry(k).or_insert(0) += v * val;
                     }
@@ -42,30 +36,31 @@ impl Solution {
                 }
                 _ => {
                     // 1. atom
-                    // 2. count: default 1
+                    // 2. count
                     let mut j = i + 1;
                     while j < sz && s[j].is_ascii_lowercase() {
                         j += 1;
                     }
-                    let atom: String = s[i..j].iter().collect();
+                    let atom: String = String::from_utf8(s[i..j].to_vec()).unwrap();
 
-                    // pull i to j
                     i = j;
-                    // consume digit
+
                     while j < sz && s[j].is_ascii_digit() {
                         j += 1;
                     }
-                    let num_str: String = s[i..j].iter().collect();
-                    let val = num_str.parse::<i32>().unwrap_or(1);
+                    let val: i32 = formula[i..j].parse().unwrap_or(1);
+
                     *stack.last_mut().unwrap().entry(atom).or_insert(0) += val;
 
                     i = j;
                 }
             }
         }
+
         let mut ret = String::new();
         for (k, v) in stack.pop().unwrap().into_iter() {
             ret.push_str(&k);
+
             if v > 1 {
                 ret.push_str(&v.to_string());
             }
