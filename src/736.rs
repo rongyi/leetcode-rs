1174e1b0433434001a98b4a45ab26a5bf81754b1
rongyi <sqrongyi@163.com>
2026-08-11 14:27:26 +0800

    Update 736

--- src/736.rs
+++ src/736.rs
@@ -1,16 +1,14 @@
-#![allow(dead_code)]
-
 struct Solution;
-use std::collections::HashMap;
 
+use std::collections::HashMap;
 #[derive(Debug, Clone)]
-enum RispExp {
-    List(Vec<RispExp>),
+enum Exp {
+    List(Vec<Exp>),
     Symbol(String),
     Number(i32),
 }
 
-fn parse(tokens: &[String]) -> (RispExp, &[String]) {
+fn parse(tokens: &[String]) -> (Exp, &[String]) {
     let (first, rest) = tokens.split_first().unwrap();
     match &first[..] {
         "(" => parse_list(rest),
@@ -18,101 +16,83 @@ fn parse(tokens: &[String]) -> (RispExp, &[String]) {
     }
 }
 
-fn parse_list(mut tokens: &[String]) -> (RispExp, &[String]) {
-    let mut ret = Vec::new();
-
+fn parse_list(mut tokens: &[String]) -> (Exp, &[String]) {
+    let mut ret: Vec<Exp> = Vec::new();
     loop {
-        match &tokens.first().unwrap()[..] {
-            ")" => {
-                return (RispExp::List(ret), &tokens[1..]);
-            }
+        match &tokens[0][..] {
+            ")" => return (Exp::List(ret), &tokens[1..]),
             _ => {
                 let (cur, rest) = parse(tokens);
-
                 ret.push(cur);
+
                 tokens = rest;
             }
         }
     }
 }
 
-fn parse_atom(token: &str) -> RispExp {
+fn parse_atom(token: &str) -> Exp {
     match token.parse::<i32>() {
-        Ok(val) => RispExp::Number(val),
-        _ => RispExp::Symbol(token.to_string()),
+        Ok(val) => Exp::Number(val),
+        _ => Exp::Symbol(token.to_string()),
     }
 }
 
-fn eval(exp: &RispExp, env: &mut Vec<HashMap<String, i32>>) -> RispExp {
+fn eval(exp: &Exp, env: &mut Vec<HashMap<String, i32>>) -> Exp {
     match exp {
-        RispExp::List(lst) => match &lst[0] {
-            RispExp::Symbol(s) => {
-                println!("here");
+        Exp::Number(v) => Exp::Number(*v),
+        Exp::Symbol(s) => {
+            for e in env.iter().rev() {
+                if e.contains_key(s) {
+                    return Exp::Number(*e.get(s).unwrap());
+                }
+            }
+            unreachable!()
+        }
+        Exp::List(lst) => {
+            if let Exp::Symbol(s) = &lst[0] {
                 match &s[..] {
                     "let" => {
-                        println!("bind");
                         env.push(HashMap::new());
                         let mut i = 1;
-                        // bind
                         while i < lst.len() - 1 {
-                            if let RispExp::Symbol(s) = &lst[i] {
-                                if let RispExp::Number(v) = eval(&lst[i + 1], env) {
+                            if let Exp::Symbol(s) = &lst[i] {
+                                if let Exp::Number(v) = eval(&lst[i + 1], env) {
                                     env.last_mut().unwrap().insert(s.to_string(), v);
                                 }
                             }
                             i += 2;
                         }
-
                         let val = eval(lst.last().unwrap(), env);
 
                         env.pop();
-                        val
+                        return val;
                     }
                     "add" => {
-                        let mut i = 1;
-                        let mut nums = Vec::new();
-                        while i < lst.len() {
-                            if let RispExp::Number(v) = eval(&lst[i], env) {
+                        let mut nums: Vec<i32> = Vec::new();
+                        for e in lst.iter().skip(1) {
+                            if let Exp::Number(v) = eval(e, env) {
                                 nums.push(v);
                             }
-
-                            i += 1;
                         }
-
-                        RispExp::Number(nums.iter().sum::<i32>())
+                        let val = nums.into_iter().sum();
+                        return Exp::Number(val);
                     }
                     "mult" => {
-                        let mut i = 1;
-                        let mut nums = Vec::new();
-                        while i < lst.len() {
-                            if let RispExp::Number(v) = eval(&lst[i], env) {
+                        let mut nums: Vec<i32> = Vec::new();
+                        for e in lst.iter().skip(1) {
+                            if let Exp::Number(v) = eval(e, env) {
                                 nums.push(v);
                             }
-
-                            i += 1;
                         }
-                        let val = nums.iter().fold(1, |acc, &cur| acc * cur);
-
-                        RispExp::Number(val)
+                        let val = nums.into_iter().fold(1, |acc, cur| acc * cur);
+                        return Exp::Number(val);
                     }
-                    _ => {
-                        unreachable!()
-                    }
+                    _ => unreachable!(),
                 }
-            }
-            _ => {
+            } else {
                 unreachable!()
             }
-        },
-        RispExp::Number(num) => RispExp::Number(*num),
-        RispExp::Symbol(s) => {
-            for e in env.iter().rev() {
-                if e.contains_key(s) {
-                    let val = e.get(s).unwrap();
-                    return RispExp::Number(*val);
-                }
-            }
-            unreachable!()
         }
     }
 }
@@ -125,23 +105,18 @@ impl Solution {
             .split_whitespace()
             .map(|s| s.to_string())
             .collect();
+        // println!("{:?}", tokens);
 
-        for token in tokens.iter() {
-            println!("{}", token);
+        let (e, _) = parse(&tokens);
+        // println!("{:?}", e);
+
+        let mut env = Vec::new();
+        if let Exp::Number(v) = eval(&e, &mut env) {
+            return v;
         }
 
-        let exp = parse(&tokens);
-        println!("{:?}", exp);
-        let mut env: Vec<HashMap<String, i32>> = Vec::new();
-        if let RispExp::Number(val) = eval(&exp.0, &mut env) {
-            return val;
-        }
         unreachable!()
     }
 }
 
-fn main() {
-    // let input = "(let x 2 (mult x (let x 3 y 4 (add x y))))".to_string();
-    let input = "(let x 3 x 2 x)".to_string();
-    Solution::evaluate(input);
-}
+fn main() {}
