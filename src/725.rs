3a2a69463eccc68e2b5aeb0f1358e61dbe6fabcb
rongyi <sqrongyi@163.com>
2026-08-11 10:29:02 +0800

    Update 725

--- src/725.rs
+++ src/725.rs
@@ -1,20 +1,4 @@
-#![allow(dead_code)]
-
 struct Solution;
-
-// Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
 
 impl Solution {
     pub fn split_list_to_parts(head: Option<Box<ListNode>>, k: i32) -> Vec<Option<Box<ListNode>>> {
