5ee3bab113f57c9e06cbf44cc4f2598415f008af
rongyi <sqrongyi@163.com>
2026-08-12 15:42:17 +0800

    Update 746

--- src/746.rs
+++ src/746.rs
@@ -1,22 +1,16 @@
-#![allow(dead_code)]
-
 struct Solution;
 
 impl Solution {
     pub fn min_cost_climbing_stairs(mut cost: Vec<i32>) -> i32 {
-        cost.push(0);
         let sz = cost.len();
         // cost to reach index i, push 0 means the top mountain
-        let mut dp = vec![0; sz];
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        let mut dp = vec![0; sz + 1];
 
-        for i in 2..sz {
-            dp[i] = dp[i - 1].min(dp[i - 2]);
-            dp[i] += cost[i];
+        for i in 2..=sz {
+            dp[i] = (dp[i - 1] + cost[i - 1]).min(dp[i - 2] + cost[i - 2]);
         }
 
-        dp[sz - 1]
+        dp[sz]
     }
 }
 
