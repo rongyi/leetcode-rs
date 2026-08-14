401034eceddb7214a4041669752f868e56c1357f
rongyi <sqrongyi@163.com>
2026-08-14 16:13:43 +0800

    Update 811

--- src/811.rs
+++ src/811.rs
@@ -1,13 +1,12 @@
-#![allow(dead_code)]
-
 struct Solution;
+
 use std::collections::HashMap;
 
 impl Solution {
     pub fn subdomain_visits(cpdomains: Vec<String>) -> Vec<String> {
         let mut group: HashMap<String, i32> = HashMap::new();
         for cp in cpdomains.into_iter() {
-            let vs: Vec<String> = cp.split(' ').map(|s| s.to_string()).collect();
+            let vs: Vec<_> = cp.split(' ').collect();
             let val = vs[0].parse::<i32>().unwrap();
             let domains: Vec<String> = vs[1].split('.').map(|s| s.to_string()).collect();
             for i in 0..domains.len() {
