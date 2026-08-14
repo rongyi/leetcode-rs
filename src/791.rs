2af61d6b531deea064b15977f6e3525ea476e6d8
rongyi <sqrongyi@163.com>
2026-08-14 10:41:41 +0800

    Update 791

--- src/791.rs
+++ src/791.rs
@@ -1,11 +1,23 @@
-#![allow(dead_code)]
 struct Solution;
 
 impl Solution {
     pub fn custom_sort_string(order: String, s: String) -> String {
-        todo!()
+        let mut score = vec![0; 26];
+        for (i, c) in order.chars().enumerate() {
+            let idx = ((c as u8) - 'a' as u8) as usize;
+            score[idx] = i;
+        }
+
+        let mut s: Vec<char> = s.chars().collect();
+
+        s.sort_by(|&l, &r| {
+            let lidx = (l as u8 - 'a' as u8) as usize;
+            let ridx = (r as u8 - 'a' as u8) as usize;
+            score[lidx].cmp(&score[ridx])
+        });
+
+        s.into_iter().collect()
     }
 }
-
 
 fn main() {}
