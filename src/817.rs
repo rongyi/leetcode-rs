37b5e0ebe9181cda036fa1d308579fc9b8cf91e2
rongyi <sqrongyi@163.com>
2026-08-15 10:37:09 +0800

    Update 817

--- src/817.rs
+++ src/817.rs
@@ -1,22 +1,6 @@
-#![allow(dead_code)]
-
-use std::collections::HashSet;
-
 struct Solution;
 
-// Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
+use std::collections::HashSet;
 
 impl Solution {
     pub fn num_components(head: Option<Box<ListNode>>, nums: Vec<i32>) -> i32 {
