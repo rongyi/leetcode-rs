30530e62d159eca4c7726f09bb802e8378033fa2
rongyi <sqrongyi@163.com>
2026-08-18 17:18:59 +0800

    Update 848

--- src/848.rs
+++ src/848.rs
@@ -1,4 +1,3 @@
-#![allow(dead_code)]
 struct Solution;
 
 impl Solution {
@@ -10,6 +9,7 @@ impl Solution {
         for i in (0..sz - 1).rev() {
             shifts[i] = (shifts[i] % 26 + shifts[i + 1]) % 26;
         }
+
         for i in 0..s.len() {
             s[i] = ((((s[i] as u8 - 'a' as u8) as i32 + shifts[i]) % 26) as u8 + 'a' as u8) as char;
         }
