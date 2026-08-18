73125dfc7abe68694e0411d1548afe12a3844cef
rongyi <sqrongyi@163.com>
2026-08-18 16:31:41 +0800

    Update 846

--- src/846.rs
+++ src/846.rs
@@ -1,8 +1,6 @@
-#![allow(dead_code)]
 struct Solution;
 
 use std::collections::BTreeMap;
-
 impl Solution {
     pub fn is_n_straight_hand(hand: Vec<i32>, group_size: i32) -> bool {
         if hand.len() % group_size as usize != 0 {
@@ -12,19 +10,19 @@ impl Solution {
         let mut cnt: BTreeMap<i32, i32> = BTreeMap::new();
 
         for &h in hand.iter() {
-            cnt.entry(h).and_modify(|v| *v += 1).or_insert(1);
+            *cnt.entry(h).or_default() += 1;
         }
 
         while !cnt.is_empty() {
             let &cur = cnt.keys().next().unwrap();
-            for w in 0..group_size {
-                let val = cnt.get(&(cur + w));
-                if val.is_none() {
+            for cur_step in 0..group_size {
+                if let Some(val) = cnt.get_mut(&(cur + cur_step)) {
+                    *val -= 1;
+                    if *val == 0 {
+                        cnt.remove(&(cur + cur_step));
+                    }
+                } else {
                     return false;
-                }
-                cnt.entry(cur + w).and_modify(|v| *v -= 1);
-                if *cnt.get(&(cur + w)).unwrap() == 0 {
-                    cnt.remove(&(cur + w));
                 }
             }
         }
