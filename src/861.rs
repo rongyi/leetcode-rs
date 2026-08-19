49469a918242824cf06e0e1ba6ea42bc40b9eb24
rongyi <sqrongyi@163.com>
2026-08-19 16:16:37 +0800

    Update 861

--- src/861.rs
+++ src/861.rs
@@ -2,26 +2,46 @@ struct Solution;
 
 impl Solution {
     pub fn matrix_score(mut grid: Vec<Vec<i32>>) -> i32 {
-        let m = grid.len();
-        let n = grid[0].len();
-        for row in grid.iter_mut() {
-            if row[0] == 0 {
-                // the first bit contribute most, so we ensure the first bit is always 1
-                for j in row.iter_mut() {
-                    *j = 1 - *j;
-                }
+        let (m, n) = (grid.len(), grid[0].len());
+
+        // make each row's highest bit be 1
+        for i in 0..m {
+            if grid[i][0] == 1 {
+                continue;
+            }
+
+            // flip current row
+            for j in 0..n {
+                grid[i][j] = 1 - grid[i][j];
             }
         }
 
-        let mut ret = (1 << (n - 1)) * m as i32;
-        for col in 1..n {
-            let mut cnt = (0..m).filter(|&row| grid[row][col] == 1).count();
-            cnt = cnt.max(m - cnt);
-            ret += (1 << (n - 1 - col)) * cnt as i32;
+        for j in 1..n {
+            // cnt how major be 1 or 0, if 0 is major, toggle this col
+            let mut one_cnt = 0;
+            for i in 0..m {
+                if grid[i][j] == 1 {
+                    one_cnt += 1;
+                }
+            }
+            // 0 is major, so toggle
+            if one_cnt < (m + 1) / 2 {
+                // toggle this col
+                for i in 0..m {
+                    grid[i][j] = 1 - grid[i][j];
+                }
+            }
         }
+        let mut ret = 0;
 
+        for i in 0..m {
+            let mut val = 0;
+            for j in 0..n {
+                val = (val << 1) + grid[i][j];
+            }
+            ret += val;
+        }
         ret
     }
 }
-
 fn main() {}
