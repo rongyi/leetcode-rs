9263cbceb86bd04ca579c01fcb350432a4138900
rongyi <sqrongyi@163.com>
2026-08-19 14:39:58 +0800

    Update 859

--- src/859.rs
+++ src/859.rs
@@ -1,26 +1,48 @@
 struct Solution;
-use std::collections::{HashMap, HashSet};
 
+use std::collections::HashMap;
 impl Solution {
     pub fn buddy_strings(s: String, goal: String) -> bool {
-        if s.len() != goal.len() {
+        let a = s.as_bytes();
+        let b = goal.as_bytes();
+        if a.len() != b.len() {
             return false;
         }
-        if s == goal {
-            let mut seen = HashSet::new();
-            return s.chars().any(|c| !seen.insert(c));
+        let sz = a.len();
+        let mut swapped = false;
+        let mut i = 0;
+        while i < sz {
+            if a[i] == b[i] {
+                i += 1;
+                continue;
+            }
+            if swapped {
+                return false;
+            }
+            let mut j = i + 1;
+            while j < sz && a[j] == b[j] {
+                j += 1;
+            }
+            // only one diff of index i
+            if j == sz {
+                return false;
+            }
+            if a[i] == b[j] && a[j] == b[i] {
+                swapped = true;
+            } else {
+                //  can not swap
+                // two diff and can not swap
+                return false;
+            }
+            i = j + 1;
         }
+        let mut cnt: HashMap<u8, usize> = HashMap::new();
+        a.iter()
+            .copied()
+            .for_each(|c| *cnt.entry(c).or_default() += 1);
 
-        let diff: Vec<(char, char)> = s
-            .chars()
-            .zip(goal.chars())
-            .filter(|&(a, b)| a != b)
-            .collect();
-
-        diff.len() == 2 && diff[0].0 == diff[1].1 && diff[0].1 == diff[1].0
+        swapped || cnt.values().any(|&v| v > 1)
     }
 }
 
-fn main() {
-    Solution::buddy_strings("ab".to_string(), "ba".to_string());
-}
+fn main() {}
