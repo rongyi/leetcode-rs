170c5e8a32fcd2c6468a93c3a88ed19a963027a3
rongyi <sqrongyi@163.com>
2026-09-01 15:42:45 +0800

    Update

--- src/886.rs
+++ src/886.rs
@@ -1,5 +1,55 @@
 struct Solution;
 
+mod ai {
+    struct Solution;
+    use std::collections::{HashMap, VecDeque};
+
+    impl Solution {
+        pub fn possible_bipartition(n: i32, dislikes: Vec<Vec<i32>>) -> bool {
+            let n = n as usize;
+            // Build adjacency list
+            let mut graph: Vec<Vec<usize>> = vec![Vec::new(); n + 1];
+            for edge in dislikes {
+                let a = edge[0] as usize;
+                let b = edge[1] as usize;
+                graph[a].push(b);
+                graph[b].push(a);
+            }
+
+            // 0 = uncolored, 1 = group A, -1 = group B
+            let mut colors: Vec<i32> = vec![0; n + 1];
+
+            // Try to color each connected component
+            for i in 1..=n {
+                if colors[i] == 0 {
+                    // Start BFS from this node
+                    colors[i] = 1;
+                    let mut queue = VecDeque::new();
+                    queue.push_back(i);
+
+                    while let Some(node) = queue.pop_front() {
+                        let current_color = colors[node];
+                        let next_color = -current_color; // Opposite color
+
+                        for &neighbor in &graph[node] {
+                            if colors[neighbor] == 0 {
+                                colors[neighbor] = next_color;
+                                queue.push_back(neighbor);
+                            } else if colors[neighbor] == current_color {
+                                // Conflict: neighbor has same color
+                                return false;
+                            }
+                            // If neighbor has opposite color, it's fine
+                        }
+                    }
+                }
+            }
+
+            true
+        }
+    }
+}
+
 use std::collections::HashMap;
 impl Solution {
     pub fn possible_bipartition(n: i32, dislikes: Vec<Vec<i32>>) -> bool {
--- src/896.rs
+++ src/896.rs
@@ -2,25 +2,23 @@ struct Solution;
 
 impl Solution {
     pub fn is_monotonic(nums: Vec<i32>) -> bool {
+        let mut is_mono_increse = true;
+        let mut is_mono_decrese = true;
         let sz = nums.len();
-        let mut increase: Option<bool> = None;
-        let mut decrease: Option<bool> = None;
+        for i in 1..sz {
+            if nums[i] < nums[i - 1] {
+                is_mono_increse = false;
+                break;
+            }
+        }
         for i in 1..sz {
             if nums[i] > nums[i - 1] {
-                if decrease.is_some() {
-                    return false;
-                }
-                increase = Some(true);
-            }
-            if nums[i] < nums[i - 1] {
-                if increase.is_some() {
-                    return false;
-                }
-                decrease = Some(true);
+                is_mono_decrese = false;
+                break;
             }
         }
 
-        true
+        is_mono_increse || is_mono_decrese
     }
 }
 
