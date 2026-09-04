9ba6263ced3981c8aebc35f1e6ea70ddc10c1521
rongyi <sqrongyi@163.com>
2026-09-04 16:00:59 +0800

    Update 931

--- src/931.rs
+++ src/931.rs
@@ -7,12 +7,15 @@ impl Solution {
         for i in 1..sz {
             for j in 0..sz {
                 let mut min_above = dp[i - 1][j];
+
                 if j > 0 {
                     min_above = min_above.min(dp[i - 1][j - 1]);
                 }
+
                 if j < sz - 1 {
                     min_above = min_above.min(dp[i - 1][j + 1]);
                 }
+
                 dp[i][j] += min_above;
             }
         }
